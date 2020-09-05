7dfffeddeeedd420575e924d0bf3e0c0e6a31619
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-09-05 12:49:44 -0400

    It compiles, but cannot find shaders or receive input. Also, only works in OpenGL

--- PortableEngine/Main.cpp
+++ PortableEngine/Main.cpp
@@ -5,11 +5,13 @@
 // --------------------------------------------------------
 // Entry point for a graphical (non-console) Windows application
 // --------------------------------------------------------
-int WINAPI WinMain(
-	_In_ HINSTANCE hInstance,		// The handle to this app's instance
-	_In_opt_ HINSTANCE hPrevInstance,	// A handle to the previous instance of the app (always NULL)
-	_In_ LPSTR lpCmdLine,			// Command line params
-	_In_ int nCmdShow)				// How the window should be shown (we ignore this)
+//int WINAPI WinMain(
+//	_In_ HINSTANCE hInstance,		// The handle to this app's instance
+//	_In_opt_ HINSTANCE hPrevInstance,	// A handle to the previous instance of the app (always NULL)
+//	_In_ LPSTR lpCmdLine,			// Command line params
+//	_In_ int nCmdShow)				// How the window should be shown (we ignore this)
+//{
+int main()
 {
 #if defined(DEBUG) | defined(_DEBUG)
 	// Enable memory leak detection as a quick and dirty
