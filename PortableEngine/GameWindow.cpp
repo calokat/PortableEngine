738fe08a68d07c5b14ddd76c66b7c00bf8949423
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-09-07 10:19:54 -0400

    Added device context to GameWindow

--- PortableEngine/GameWindow.cpp
+++ PortableEngine/GameWindow.cpp
@@ -1,5 +1,7 @@
 #include "GameWindow.h"
 
-GameWindow::GameWindow(unsigned int x, unsigned int y, unsigned int width, unsigned int height, int whnd) : x(x), y(y), width(width), height(height), windowHandle(whnd)
+GameWindow::GameWindow(unsigned int x, unsigned int y, unsigned int width, unsigned int height) : x(x), y(y), width(width), height(height)
 {
+	windowHandle = -1;
+	deviceContext = -1;
 }
--- PortableEngine/GameWindow.h
+++ PortableEngine/GameWindow.h
@@ -6,6 +6,7 @@ struct GameWindow
 	unsigned int width;
 	unsigned int height;
 	int windowHandle;
-	GameWindow(unsigned int x, unsigned int y, unsigned int width, unsigned int height, int whnd);
+	int deviceContext;
+	GameWindow(unsigned int x, unsigned int y, unsigned int width, unsigned int height);
 };
 
