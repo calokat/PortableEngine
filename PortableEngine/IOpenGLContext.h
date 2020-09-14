a02cb8ce2c5892519ea965b7acd27537c337e4d7
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-09-14 11:45:51 -0400

    Fixed GetContext signature to match those of its subclasses

--- PortableEngine/IOpenGLContext.h
+++ PortableEngine/IOpenGLContext.h
@@ -1,8 +1,9 @@
 #pragma once
+#include "GameWindow.h"
 class IOpenGLContext
 {
 public:
-	virtual int GetContext() = 0;
+	virtual int GetContext(GameWindow*) = 0;
 	virtual void _SwapBuffers(int winHandle) = 0;
 };
 
