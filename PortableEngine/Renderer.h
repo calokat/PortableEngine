697d4473e094c21dcda029a309db46b414aee5d5
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-09-24 10:50:15 -0400

    Added Renderer class

---
+++ PortableEngine/Renderer.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Shader.h"
+//#include "IGraphicsAPI.h"
+#include "IPlatform.h"
+#include <string>
+#include <vector>
+#include "Vertex.h"
+// source: https://github.com/IGME-RIT/Basic-OpenGL-with-GLFW-Assimp/blob/master/OpenGLObjectLoading/mesh.cpp
+#define SetupAttribute(index, size, type, structure, element) \
+	glVertexAttribPointer(index, size, type, 0, sizeof(structure), (void*)offsetof(structure, element)); \
+
+
+class Renderer
+{
+private:
+	Shader* vertex;
+	Shader* pixel;
+	//IGraphicsAPI* glAPI;
+	GLuint program;
+	GLuint vao, vbo, ibo;
+	GLint projLoc, viewLoc, modelLoc;
+	int numVertices;
+public:
+	Renderer(IPlatform*);
+	void LoadMesh(std::vector<Vertex>);
+	void Draw();
+	~Renderer();
+};
