a147f1d9499840862a703e317f4a07088efa7a16
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-09-28 18:45:11 -0400

    Context creation is more advanced

--- PortableEngine/WinOpenGLContext.cpp
+++ PortableEngine/WinOpenGLContext.cpp
@@ -1,79 +1,71 @@
+#ifdef _WIN64
 #include "WinOpenGLContext.h"
 #include <iostream>
 
 int WinOpenGLContext::GetContext(GameWindow* window)
 {
-	PIXELFORMATDESCRIPTOR pfd =
-	{
-		sizeof(PIXELFORMATDESCRIPTOR),
-		1,
-		PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    // Flags
-		PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
-		32,                   // Colordepth of the framebuffer.
-		0, 0, 0, 0, 0, 0,
-		0,
-		0,
-		0,
-		0, 0, 0, 0,
-		24,                   // Number of bits for the depthbuffer
-		8,                    // Number of bits for the stencilbuffer
-		0,                    // Number of Aux buffers in the framebuffer.
-		PFD_MAIN_PLANE,
-		0,
-		0, 0, 0
-	};
-
-	int formatID = ChoosePixelFormat((HDC)window->deviceContext, &pfd);
-	BOOL res = SetPixelFormat((HDC)window->deviceContext, formatID, &pfd);
-
-	HGLRC rc = wglCreateContext((HDC)window->deviceContext);
-	res = wglMakeCurrent((HDC)window->deviceContext, rc);
-
-	// Thanks to https://mariuszbartosik.com/opengl-4-x-initialization-in-windows-without-a-framework/
-	//const int pixelAttribs[] = {
-	//WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
-	//WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
-	//WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
-	//WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
-	//WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
-	//WGL_COLOR_BITS_ARB, 32,
-	//WGL_ALPHA_BITS_ARB, 8,
-	//WGL_DEPTH_BITS_ARB, 24,
-	//WGL_STENCIL_BITS_ARB, 8,
-	//WGL_SAMPLE_BUFFERS_ARB, GL_TRUE,
-	//WGL_SAMPLES_ARB, 4,
-	//0
-	//};
-
-	//int pixelFormatID; UINT numFormats;
-	//bool status = wglChoosePixelFormatARB((HDC)window->deviceContext, pixelAttribs, NULL, 1, &pixelFormatID, &numFormats);
-
-	//if (status == false || numFormats == 0)
+	//PIXELFORMATDESCRIPTOR pfd =
 	//{
-	//	printf("wglChoosePixelFormatARB() failed.");
-	//	return 1;
-	//}
-
-	//PIXELFORMATDESCRIPTOR PFD;
-	//DescribePixelFormat((HDC)window->deviceContext, pixelFormatID, sizeof(PFD), &PFD);
-	//SetPixelFormat((HDC)window->deviceContext, pixelFormatID, &PFD);
-
-	//const int major_min = 4, minor_min = 5;
-	//int  contextAttribs[] = {
-	//	WGL_CONTEXT_MAJOR_VERSION_ARB, major_min,
-	//	WGL_CONTEXT_MINOR_VERSION_ARB, minor_min,
-	//	WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
-	//	0
+	//	sizeof(PIXELFORMATDESCRIPTOR),
+	//	1,
+	//	PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    // Flags
+	//	PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
+	//	32,                   // Colordepth of the framebuffer.
+	//	0, 0, 0, 0, 0, 0,
+	//	0,
+	//	0,
+	//	0,
+	//	0, 0, 0, 0,
+	//	24,                   // Number of bits for the depthbuffer
+	//	8,                    // Number of bits for the stencilbuffer
+	//	0,                    // Number of Aux buffers in the framebuffer.
+	//	PFD_MAIN_PLANE,
+	//	0,
+	//	0, 0, 0
 	//};
-	//HGLRC RC;
-	//RC = wglCreateContextAttribsARB((HDC)window->deviceContext, 0, contextAttribs);
-	//if (RC == NULL) {
-	//	printf("wglCreateContextAttribsARB() failed.");
-	//	return 1;
-	//}
 
-	//wglMakeCurrent((HDC)window->deviceContext, RC);
+	//int formatID = ChoosePixelFormat((HDC)window->deviceContext, &pfd);
+	//BOOL res = SetPixelFormat((HDC)window->deviceContext, formatID, &pfd);
 
+	//HGLRC rc = wglCreateContext((HDC)window->deviceContext);
+	//res = wglMakeCurrent((HDC)window->deviceContext, rc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	PIXELFORMATDESCRIPTOR pfd;
+	memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 32;
+	pfd.iLayerType = PFD_MAIN_PLANE;
+
+	int nPixelFormat = ChoosePixelFormat(*(HDC*)platform->GetDeviceContext(), &pfd);
+
+	BOOL bResult = SetPixelFormat(*(HDC*)platform->GetDeviceContext(), nPixelFormat, &pfd);
+
+	if (!bResult) return false;
+
+	HGLRC tempContext = wglCreateContext(*(HDC*)platform->GetDeviceContext());
+	wglMakeCurrent(*(HDC*)platform->GetDeviceContext(), tempContext);
 
 	glewExperimental = true;
 	GLenum  glewStatus = glewInit();
@@ -82,10 +74,35 @@ int WinOpenGLContext::GetContext(GameWindow* window)
 		printf("GLEW initialization failed");
 		return -1;
 	}
+
+	int attribs[] =
+	{
+		WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+		WGL_CONTEXT_MINOR_VERSION_ARB, 1,
+		WGL_CONTEXT_FLAGS_ARB, 0,
+		0
+	};
+
+	m_hrc = wglCreateContextAttribsARB(*(HDC*)platform->GetDeviceContext(), 0, attribs);
+	wglMakeCurrent(NULL, NULL);
+	wglDeleteContext(tempContext);
+	wglMakeCurrent(*(HDC*)platform->GetDeviceContext(), m_hrc);
+
+	if (!m_hrc) return -1;
+
+
 	return 0;
 }
 
 void WinOpenGLContext::_SwapBuffers(int winHandle)
 {
-	SwapBuffers((HDC)winHandle);
+	//if (SwapBuffers(*(HDC*)platform->GetDeviceContext()))
+	//	printf("Whoopee!\n");
+	//else
+	//	printf("Boo-hoo\n");
+	SwapBuffers(*(HDC*)platform->GetDeviceContext());
 }
+WinOpenGLContext::WinOpenGLContext(IPlatform* plat) : platform(plat)
+{
+}
+#endif
