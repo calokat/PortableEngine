3ddddd267299d1551b6f7f74d2b7c9fb1dabbb1b
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-10-07 11:34:45 -0400

    Added RenderSystem.h

---
+++ PortableEngine/RenderSystem.h
@@ -0,0 +1,4 @@
+#pragma once
+// source: https://github.com/IGME-RIT/Basic-OpenGL-with-GLFW-Assimp/blob/master/OpenGLObjectLoading/mesh.cpp
+#define SetupAttribute(index, size, type, structure, element) \
+	glVertexAttribPointer(index, size, type, 0, sizeof(structure), (void*)offsetof(structure, element)); \
