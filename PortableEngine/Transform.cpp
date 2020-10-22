3065ed5c3ff92ce184c428f227be4899de70a2f4
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-10-22 11:52:47 -0400

    Transform: Added assignment operator overload. Removed methods, they are now in TransformSystem

--- PortableEngine/Transform.cpp
+++ PortableEngine/Transform.cpp
@@ -9,105 +9,117 @@ Transform::Transform()
 	this->worldMatrix = glm::mat4(1.0f);
 }
 
-void Transform::SetPosition(glm::vec3 newPos)
-{
-	position = newPos;
-}
-
-void Transform::SetRotation(glm::vec3 newRot)
-{
-	rotation = newRot;
-}
-
-void Transform::SetScale(glm::vec3 newScale)
-{
-	scale = newScale;
-}
-
-glm::vec3 Transform::GetPosition()
-{
-	return position;
-}
-
-glm::vec3 Transform::GetRotation()
-{
-	return rotation;
-}
-
-glm::vec3 Transform::GetScale()
-{
-	return scale;
-}
-
-glm::mat4 Transform::GetWorldMatrix()
-{
-	glm::mat4 translationMat = glm::translate(glm::mat4(1.0), position);
-	glm::mat4 rotationMat = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
-	glm::mat4 scaleMat = glm::scale(glm::mat4(1.0f), scale);
-	worldMatrix = scaleMat * rotationMat * translationMat;
-	return worldMatrix;
-}
-
-void Transform::MoveAbsolute(glm::vec3 delta)
-{
-	position = position + delta;
-}
-
-void Transform::MoveRelative(glm::vec3 unrotated)
-{
-	glm::vec4 unrotatedVec(unrotated.x, unrotated.y, unrotated.z, 0);
-	glm::mat4 rotationMat = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
-	glm::vec3 rotatedVec = rotationMat * unrotatedVec;
-	position = position + rotatedVec;
-}
-
-void Transform::Rotate(glm::vec3 deltaRot)
-{
-	rotation += deltaRot;
-	GetWorldMatrix();
-}
-
-void Transform::Scale(glm::vec3 deltaScale)
-{
-	scale += deltaScale;
-}
-
-void Transform::RotateAroundPoint(glm::vec3 point, float angle)
-{
-	float distance = sqrtf(powf(position.x - point.x, 2) + powf(position.y - point.y, 2) + powf(position.z - point.z, 2));
-	glm::vec3 unitDir(cos(angle), sin(angle), 0);
-	glm::vec3 dir = distance * unitDir;
-	position = dir + point;
-
-}
-
-glm::vec3 Transform::GetForward()
-{
-	//glm::vec3 globalFwd(0, 0, 1);
-	//glm::quat
-	//glm::quat rotQuat = (rotation.x, rotation.y, rotation.z);
-	//glm::vec3 localFwd = XMVector3Rotate(globalFwd, rotQuat);
-
-	//glm::vec3 result;
-	//result = localFwd;
-	//return result;
-	//glm::mat4 rotationMat = glm::rotate(glm::mat4(1.0f), rotation.x, glm::vec3(1, 0, 0)) * glm::rotate(glm::mat4(1.0f), rotation.y, glm::vec3(0, 1, 0)) * glm::rotate(glm::mat4(1.0f), rotation.z, glm::vec3(0, 0, 1));
-	//glm::vec4 vec4Result = rotationMat * glm::vec4(0, 0, 1, 0);
-	//return glm::vec3(vec4Result.x, vec4Result.y, vec4Result.z);
-	
-	
-	
-	return glm::vec3(worldMatrix[2].x, worldMatrix[2].y, worldMatrix[2].z);
-	
-	
-	// WHY DOESN'T THIS WORK
-	/*glm::vec4 globalFwd(0, 0, 1, 0);
-	glm::vec4 localFwd = globalFwd * glm::yawPitchRoll(-rotation.y, -rotation.x, -rotation.z);
-	return glm::vec3(localFwd.x, localFwd.y, localFwd.z);*/
-
-}
+//void Transform::SetPosition(glm::vec3 newPos)
+//{
+//	position = newPos;
+//}
+//
+//void Transform::SetRotation(glm::vec3 newRot)
+//{
+//	rotation = newRot;
+//}
+//
+//void Transform::SetScale(glm::vec3 newScale)
+//{
+//	scale = newScale;
+//}
+//
+//glm::vec3 Transform::GetPosition()
+//{
+//	return position;
+//}
+//
+//glm::vec3 Transform::GetRotation()
+//{
+//	return rotation;
+//}
+//
+//glm::vec3 Transform::GetScale()
+//{
+//	return scale;
+//}
+//
+//glm::mat4 Transform::GetWorldMatrix()
+//{
+//	glm::mat4 translationMat = glm::translate(glm::mat4(1.0), position);
+//	glm::mat4 rotationMat = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+//	glm::mat4 scaleMat = glm::scale(glm::mat4(1.0f), scale);
+//	worldMatrix = scaleMat * rotationMat * translationMat;
+//	return worldMatrix;
+//}
+//
+//void Transform::MoveAbsolute(glm::vec3 delta)
+//{
+//	position = position + delta;
+//}
+//
+//void Transform::MoveRelative(glm::vec3 unrotated)
+//{
+//	glm::vec4 unrotatedVec(unrotated.x, unrotated.y, unrotated.z, 0);
+//	glm::mat4 rotationMat = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+//	glm::vec3 rotatedVec = rotationMat * unrotatedVec;
+//	position = position + rotatedVec;
+//}
+//
+//void Transform::Rotate(glm::vec3 deltaRot)
+//{
+//	rotation += deltaRot;
+//	GetWorldMatrix();
+//}
+//
+//void Transform::Scale(glm::vec3 deltaScale)
+//{
+//	scale += deltaScale;
+//}
+//
+//void Transform::RotateAroundPoint(glm::vec3 point, float angle)
+//{
+//	float distance = sqrtf(powf(position.x - point.x, 2) + powf(position.y - point.y, 2) + powf(position.z - point.z, 2));
+//	glm::vec3 unitDir(cos(angle), sin(angle), 0);
+//	glm::vec3 dir = distance * unitDir;
+//	position = dir + point;
+//
+//}
+//
+//glm::vec3 Transform::GetForward()
+//{
+//	//glm::vec3 globalFwd(0, 0, 1);
+//	//glm::quat
+//	//glm::quat rotQuat = (rotation.x, rotation.y, rotation.z);
+//	//glm::vec3 localFwd = XMVector3Rotate(globalFwd, rotQuat);
+//
+//	//glm::vec3 result;
+//	//result = localFwd;
+//	//return result;
+//	//glm::mat4 rotationMat = glm::rotate(glm::mat4(1.0f), rotation.x, glm::vec3(1, 0, 0)) * glm::rotate(glm::mat4(1.0f), rotation.y, glm::vec3(0, 1, 0)) * glm::rotate(glm::mat4(1.0f), rotation.z, glm::vec3(0, 0, 1));
+//	//glm::vec4 vec4Result = rotationMat * glm::vec4(0, 0, 1, 0);
+//	//return glm::vec3(vec4Result.x, vec4Result.y, vec4Result.z);
+//	
+//	
+//	
+//	return glm::vec3(worldMatrix[2].x, worldMatrix[2].y, worldMatrix[2].z);
+//	
+//	
+//	// WHY DOESN'T THIS WORK
+//	/*glm::vec4 globalFwd(0, 0, 1, 0);
+//	glm::vec4 localFwd = globalFwd * glm::yawPitchRoll(-rotation.y, -rotation.x, -rotation.z);
+//	return glm::vec3(localFwd.x, localFwd.y, localFwd.z);*/
+//
+//}
 
 Transform& Transform::operator=(Transform&& other)
+{
+	if (this != &other)
+	{
+		this->worldMatrix = other.worldMatrix;
+		this->position = other.position;
+		this->rotation = other.rotation;
+		this->scale = other.scale;
+	}
+	return *this;
+}
+
+Transform& Transform::operator=(Transform& other)
 {
 	if (this != &other)
 	{
--- PortableEngine/Transform.h
+++ PortableEngine/Transform.h
@@ -5,27 +5,28 @@
 #include "glm/gtc/type_ptr.hpp"
 class Transform
 {
-	glm::mat4 worldMatrix;
-	glm::vec3 position;
-	glm::vec3 rotation;
-	glm::vec3 scale;
-
 	public:
+		glm::mat4 worldMatrix;
+		glm::vec3 position;
+		glm::vec3 rotation;
+		glm::vec3 scale;
+
 		Transform();
-		void SetPosition(glm::vec3 newPos);
-		void SetRotation(glm::vec3 newRot);
-		void SetScale(glm::vec3 newScale);
-		glm::vec3 GetPosition();
-		glm::vec3 GetRotation();
-		glm::vec3 GetScale();
-		glm::mat4 GetWorldMatrix();
-		void MoveAbsolute(glm::vec3);
-		void MoveRelative(glm::vec3);
-		void Rotate(glm::vec3);
-		void Scale(glm::vec3);
-		void RotateAroundPoint(glm::vec3 point, float angle);
-		glm::vec3 GetForward();
+		//void SetPosition(glm::vec3 newPos);
+		//void SetRotation(glm::vec3 newRot);
+		//void SetScale(glm::vec3 newScale);
+		//glm::vec3 GetPosition();
+		//glm::vec3 GetRotation();
+		//glm::vec3 GetScale();
+		//glm::mat4 GetWorldMatrix();
+		//void MoveAbsolute(glm::vec3);
+		//void MoveRelative(glm::vec3);
+		//void Rotate(glm::vec3);
+		//void Scale(glm::vec3);
+		//void RotateAroundPoint(glm::vec3 point, float angle);
+		//glm::vec3 GetForward();
 		Transform& operator=(Transform&& other);
+		Transform& operator=(Transform& other);
 		Transform(Transform&& other) noexcept;
 		Transform(Transform& other);
 };
