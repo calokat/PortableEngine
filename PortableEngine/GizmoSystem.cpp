9be607616020ad63ae5908355cd47ad15915cbae
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-10-23 14:10:48 -0400

    Fleshed out GizmoSystem. Of note, a vector of Transform* is used for selected objects, and the gizmo operation enum field is public.

--- PortableEngine/GizmoSystem.cpp
+++ PortableEngine/GizmoSystem.cpp
@@ -1,1 +1,37 @@
 #include "GizmoSystem.h"
+
+std::vector<Transform*> GizmoSystem::selected;
+ImGuizmo::OPERATION GizmoSystem::op;
+void GizmoSystem::DrawGizmo(Camera camera)
+{
+	if (selected.empty())
+	{
+		return;
+	}
+	ImGuiIO& io = ImGui::GetIO();
+	ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
+	ImGuizmo::Manipulate(glm::value_ptr(camera.view), glm::value_ptr(camera.projection), op, (op == ImGuizmo::SCALE) ? ImGuizmo::LOCAL : ImGuizmo::WORLD, glm::value_ptr(selected[0]->worldMatrix));
+
+}
+
+void GizmoSystem::Select(Transform* toSelect)
+{
+	selected.push_back(toSelect);
+}
+
+void GizmoSystem::Deselect(Transform* toDeselect)
+{
+	for (auto it = selected.begin(); it != selected.end(); ++it)
+	{
+		if (*it == toDeselect)
+		{
+			selected.erase(it);
+			return;
+		}
+	}
+}
+
+void GizmoSystem::DeselectAll()
+{
+	selected.clear();
+}
--- PortableEngine/GizmoSystem.h
+++ PortableEngine/GizmoSystem.h
@@ -1,5 +1,19 @@
 #pragma once
+#include "Camera.h"
+#include <imgui.h>
+#include <ImGuizmo.h>
+#include "Mesh.h"
+#include <vector>
 class GizmoSystem
 {
+private:
+	// TODO: Allow for multiple things to be selected
+	static std::vector<Transform*> selected;
+public:
+	static void DrawGizmo(Camera camera);
+	static void Select(Transform* toSelect);
+	static void Deselect(Transform* toDeselect);
+	static void DeselectAll();
+	static ImGuizmo::OPERATION op;
 };
 
