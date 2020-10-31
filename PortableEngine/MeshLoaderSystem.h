033e9f0d7743d4877a4838bcba5fd68eba0317a6
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-10-31 11:37:29 -0400

    Put Mesh loading logic into MeshLoaderSystem

--- PortableEngine/MeshLoaderSystem.h
+++ PortableEngine/MeshLoaderSystem.h
@@ -1,5 +1,10 @@
 #pragma once
+#include "Vertex.h"
+#include "Mesh.h"
 class MeshLoaderSystem
 {
+public:
+	static void LoadMesh(const char* fileName, Mesh& m);
+	static void CalculateTangents(Vertex* verts, int numVerts, unsigned int* indices, int numIndices);
 };
 
