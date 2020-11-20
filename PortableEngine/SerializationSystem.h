7471db4857e9ae562300995e2eac729285ec5f34
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-11-19 22:45:43 -0500

    Added deserialization for glm::vec4

--- PortableEngine/SerializationSystem.h
+++ PortableEngine/SerializationSystem.h
@@ -19,6 +19,20 @@ void from_json(const nlohmann::json& j, glm::vec3& vec)
 	j.at("z").get_to(vec.z);
 }
 
+void to_json(nlohmann::json& j, const glm::vec4& vec)
+{
+	j = nlohmann::json{ {"x", vec.x}, {"y", vec.y}, {"z", vec.z}, {"w", vec.w} };
+}
+
+void from_json(const nlohmann::json& j, glm::vec4& vec)
+{
+	j.at("x").get_to(vec.x);
+	j.at("y").get_to(vec.y);
+	j.at("z").get_to(vec.z);
+	j.at("w").get_to(vec.w);
+}
+
+
 void to_json(nlohmann::json& j, const glm::mat4& mat)
 {
 	j = nlohmann::json
