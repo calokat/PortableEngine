c3538443fc2b25b886a3649f5417bfac6f043625
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-11-27 22:07:17 -0500

    Commented out all ImGui code, including gizmo code, that would interfere with making DirectX rendering happen. Commented out serialization code to remove distractions

--- PortableEngine/Main.cpp
+++ PortableEngine/Main.cpp
@@ -9,28 +9,28 @@
 #include "EmscriptenPlatform.h"
 #include <emscripten/html5.h>
 #endif
-#include "OpenGLAPI.h"
+//#include "OpenGLAPI.h"
 #include "PortableGame.h"
 #include "Mesh.h"
-#include "Renderer.h"
-#include "RenderSystem.h"
+//#include "Renderer.h"
+//#include "RenderSystem.h"
 #include "Camera.h"
 #include <entt.hpp>
-#include <imgui.h>
-#include <examples/imgui_impl_win32.h>
-#include <examples/imgui_impl_opengl3.h>
-#include <ImGuizmo.h>
+//#include <imgui.h>
+//#include <examples/imgui_impl_win32.h>
+//#include <examples/imgui_impl_opengl3.h>
+//#include <ImGuizmo.h>
 #include "TransformSystem.h"
 #include "CameraSystem.h"
-#include "GizmoSystem.h"
+//#include "GizmoSystem.h"
 #include "MeshLoaderSystem.h"
 #include <typeinfo>
 #include <map>
 #include <random>
 #include <json.hpp>
 #include <iomanip>
-#include "InspectorGUI.h"
-#include "SerializationSystem.h"
+//#include "InspectorGUI.h"
+//#include "SerializationSystem.h"
 #include "misc_components.h"
 // Thanks to https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c
 #include <filesystem>
@@ -49,109 +49,109 @@ entt::registry registry;
 
 bool show_demo_window = true;
 
-template<class T>
-void TrySerializeComponent(json& master)
-{
-	std::string typeName = typeid(T).name();
-	//std::ofstream jsonFile = std::ofstream(typeName);
-	nlohmann::json json;
-	registry.each([&json](entt::entity e) {
-		if (registry.has<T>(e))
-		{
-			T& obj = registry.get<T>(e);
-			std::string stringified = std::to_string((int)e);
-			json[stringified] = obj;
-		}
-	});
-	//jsonFile << json << std::endl;
-	master[typeName] = json;
-}
-void Serialize(const char* saveFileName)
-{
-	json saveJson;
-	json entitiesJson;
-	std::vector<std::string> stringifiedEntities;
-	registry.each([&stringifiedEntities](entt::entity e)
-	{
-		stringifiedEntities.push_back(std::to_string((int)e));
-	});
-	entitiesJson = stringifiedEntities;
-	//std::ofstream entitiesFile("entities.json");
-	//entitiesFile << entitiesJson << std::endl;
-	saveJson["entities"] = entitiesJson;
-	TrySerializeComponent<Transform>(saveJson);
-	TrySerializeComponent<Mesh>(saveJson);
-	TrySerializeComponent<Camera>(saveJson);
-	TrySerializeComponent<Renderer>(saveJson);
-	TrySerializeComponent<Name>(saveJson);
-	TrySerializeComponent<Rotator>(saveJson);
-	char fullSavePath[150];
-	strcpy_s(fullSavePath, saveFileName);
-	strcat_s(fullSavePath, ".pg");
-	std::ofstream saveFile(fullSavePath);
-	saveFile << std::setw(4) << saveJson << std::endl;
-}
-template<class T>
-void TryDeserializeComponent(std::map<std::string, entt::entity> entityMap, std::vector<std::string> storedEntities, json master)
-{
-	//std::string g = std::string(typeid(T).name()).append(".json");
-	//std::ifstream inFile(g);
-	//json objJson;
-	//inFile >> objJson;
-	json entitiesJson = master["entities"];
-	const char* className = typeid(T).name();
-	json objJson = master[className];
-	for (auto it = storedEntities.begin(); it != storedEntities.end(); ++it)
-	{
-		if (objJson.find(*it) != objJson.end())
-		{
-			auto compIter = objJson[*it];
-			T obj;
-			from_json(compIter, obj);
-			registry.emplace<T>(entityMap[*it], std::move(obj));
-		}
-	}
-}
-void Deserialize(const char* saveFileName)
-{
-	GizmoSystem::DeselectAll();
-	registry.clear();
-	std::vector<std::string> storedEntities;
-	std::ifstream saveFile(saveFileName);
-	json saveJson;
-	saveFile >> saveJson;
-	json entitiesJson = saveJson["entities"];
-	storedEntities = entitiesJson.get<std::vector<std::string>>();
-	std::map<std::string, entt::entity> entityMap;
-	for (auto it = storedEntities.begin(); it != storedEntities.end(); ++it)
-	{
-		entt::entity e = registry.create();
-		entityMap.emplace(*it, e);
-	}
-	TryDeserializeComponent<Mesh>(entityMap, storedEntities, saveJson);
-	TryDeserializeComponent<Camera>(entityMap, storedEntities, saveJson);
-	TryDeserializeComponent<Renderer>(entityMap, storedEntities, saveJson);
-	TryDeserializeComponent<Transform>(entityMap, storedEntities, saveJson);
-	TryDeserializeComponent<Name>(entityMap, storedEntities, saveJson);
-	TryDeserializeComponent<Rotator>(entityMap, storedEntities, saveJson);
-	auto compView = registry.view<Mesh>();
-	for (auto v : compView)
-	{
-		Mesh& m = registry.get<Mesh>(v);
-		MeshLoaderSystem::LoadMesh(m.path.c_str(), m);
-	}
-	auto rendererView = registry.view<Renderer>();
-	auto cameraView = registry.view<Camera>();
-	Camera& camera = registry.get<Camera>(cameraView[0]);
-	for (auto rv : rendererView)
-	{
-		Renderer& renderer = registry.get<Renderer>(rv);
-		Load(renderer, camera);
-	}
-	auto meshView = registry.view<Mesh, Transform>();
-	Transform& meshTransform = registry.get<Transform>(*meshView.begin());
-	GizmoSystem::Select(*meshView.begin());
-}
+//template<class T>
+//void TrySerializeComponent(json& master)
+//{
+//	std::string typeName = typeid(T).name();
+//	//std::ofstream jsonFile = std::ofstream(typeName);
+//	nlohmann::json json;
+//	registry.each([&json](entt::entity e) {
+//		if (registry.has<T>(e))
+//		{
+//			T& obj = registry.get<T>(e);
+//			std::string stringified = std::to_string((int)e);
+//			json[stringified] = obj;
+//		}
+//	});
+//	//jsonFile << json << std::endl;
+//	master[typeName] = json;
+//}
+//void Serialize(const char* saveFileName)
+//{
+//	json saveJson;
+//	json entitiesJson;
+//	std::vector<std::string> stringifiedEntities;
+//	registry.each([&stringifiedEntities](entt::entity e)
+//	{
+//		stringifiedEntities.push_back(std::to_string((int)e));
+//	});
+//	entitiesJson = stringifiedEntities;
+//	//std::ofstream entitiesFile("entities.json");
+//	//entitiesFile << entitiesJson << std::endl;
+//	saveJson["entities"] = entitiesJson;
+//	TrySerializeComponent<Transform>(saveJson);
+//	TrySerializeComponent<Mesh>(saveJson);
+//	TrySerializeComponent<Camera>(saveJson);
+//	TrySerializeComponent<Renderer>(saveJson);
+//	TrySerializeComponent<Name>(saveJson);
+//	TrySerializeComponent<Rotator>(saveJson);
+//	char fullSavePath[150];
+//	strcpy_s(fullSavePath, saveFileName);
+//	strcat_s(fullSavePath, ".pg");
+//	std::ofstream saveFile(fullSavePath);
+//	saveFile << std::setw(4) << saveJson << std::endl;
+//}
+//template<class T>
+//void TryDeserializeComponent(std::map<std::string, entt::entity> entityMap, std::vector<std::string> storedEntities, json master)
+//{
+//	//std::string g = std::string(typeid(T).name()).append(".json");
+//	//std::ifstream inFile(g);
+//	//json objJson;
+//	//inFile >> objJson;
+//	json entitiesJson = master["entities"];
+//	const char* className = typeid(T).name();
+//	json objJson = master[className];
+//	for (auto it = storedEntities.begin(); it != storedEntities.end(); ++it)
+//	{
+//		if (objJson.find(*it) != objJson.end())
+//		{
+//			auto compIter = objJson[*it];
+//			T obj;
+//			from_json(compIter, obj);
+//			registry.emplace<T>(entityMap[*it], std::move(obj));
+//		}
+//	}
+//}
+//void Deserialize(const char* saveFileName)
+//{
+//	//GizmoSystem::DeselectAll();
+//	registry.clear();
+//	std::vector<std::string> storedEntities;
+//	std::ifstream saveFile(saveFileName);
+//	json saveJson;
+//	saveFile >> saveJson;
+//	json entitiesJson = saveJson["entities"];
+//	storedEntities = entitiesJson.get<std::vector<std::string>>();
+//	std::map<std::string, entt::entity> entityMap;
+//	for (auto it = storedEntities.begin(); it != storedEntities.end(); ++it)
+//	{
+//		entt::entity e = registry.create();
+//		entityMap.emplace(*it, e);
+//	}
+//	TryDeserializeComponent<Mesh>(entityMap, storedEntities, saveJson);
+//	TryDeserializeComponent<Camera>(entityMap, storedEntities, saveJson);
+//	TryDeserializeComponent<Renderer>(entityMap, storedEntities, saveJson);
+//	TryDeserializeComponent<Transform>(entityMap, storedEntities, saveJson);
+//	TryDeserializeComponent<Name>(entityMap, storedEntities, saveJson);
+//	TryDeserializeComponent<Rotator>(entityMap, storedEntities, saveJson);
+//	auto compView = registry.view<Mesh>();
+//	for (auto v : compView)
+//	{
+//		Mesh& m = registry.get<Mesh>(v);
+//		MeshLoaderSystem::LoadMesh(m.path.c_str(), m);
+//	}
+//	auto rendererView = registry.view<Renderer>();
+//	auto cameraView = registry.view<Camera>();
+//	Camera& camera = registry.get<Camera>(cameraView[0]);
+//	for (auto rv : rendererView)
+//	{
+//		Renderer& renderer = registry.get<Renderer>(rv);
+//		Load(renderer, camera);
+//	}
+//	auto meshView = registry.view<Mesh, Transform>();
+//	Transform& meshTransform = registry.get<Transform>(*meshView.begin());
+//	//GizmoSystem::Select(*meshView.begin());
+//}
 
 void MakeMesh(const char* path, const char* name = "GameObject") {
 	auto camView = registry.view<Camera>();
@@ -191,8 +191,9 @@ void MakeRayFromCamera()
 {
 	auto camView = registry.view<Camera>();
 	auto [camera, camTransform] = registry.get<Camera, Transform>(camView[0]);
-	ImGuiIO& io = ImGui::GetIO();
-	ImVec2 mousePos = io.MousePos;
+	//ImGuiIO& io = ImGui::GetIO();
+	//ImVec2 mousePos = io.MousePos;
+	glm::vec2 mousePos = plat->GetInputSystem()->GetCursorPosition();
 	// Blessed be this code taken from https://gamedev.stackexchange.com/questions/157674/simple-mouseray-picking-in-opengl
 	glm::vec3 mouse_world_nearplane = glm::unProject(
 		glm::vec3(mousePos.x, 600 - mousePos.y, 0.0f),
@@ -247,14 +248,14 @@ void MakeRayFromCamera()
 
 void Loop()
 {
-	ImGui_ImplOpenGL3_NewFrame();
+	//ImGui_ImplOpenGL3_NewFrame();
 	//ImGui_ImplWin32_NewFrame();
 	plat->NewGuiFrame();
-	ImGui::NewFrame();
-	ImGuizmo::BeginFrame();
-	ImGuizmo::Enable(true);
+	//ImGui::NewFrame();
+	//ImGuizmo::BeginFrame();
+	//ImGuizmo::Enable(true);
 	static float vertColorPick[4];
-	ImGui::BeginMainMenuBar();
+	/*ImGui::BeginMainMenuBar();
 	if (ImGui::BeginMenu("File"))
 	{
 		if (ImGui::BeginMenu("New"))
@@ -312,7 +313,7 @@ void Loop()
 			ImGui::EndMenu();
 		}
 		ImGui::EndMenu();
-	}
+	}*/
 	//if (ImGui::BeginMenu("Components"))
 	//{
 	//	if (ImGui::BeginMenu("Color picker"))
@@ -326,7 +327,7 @@ void Loop()
 	//	}
 	//	ImGui::EndMenu();
 	//}
-	ImGui::EndMainMenuBar();
+	//ImGui::EndMainMenuBar();
 	//ImGui::Begin("Vertex color picker", NULL, ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoMove);
 	//ImGui::Text("Vertex color: ");
 	//ImGui::ColorPicker4("Vertex color: ", vertColorPick);
@@ -334,52 +335,52 @@ void Loop()
 
 	plat->GetInputSystem()->GetKeyPressed();
 	auto view = registry.view<Mesh, Renderer, Transform, Name>();
-	ImGui::Begin("Entity List");
-	ImGui::SetWindowPos({ 0, 20 });
-	ImGui::SetWindowSize({ 200, 780 });
-	for (auto renderable : view)
-	{
-		Name name = view.get<Name>(renderable);
-		if (ImGui::MenuItem(name.nameString.c_str()))
-		{
-			GizmoSystem::DeselectAll();
-			Transform& newSelected = view.get<Transform>(renderable);
-			GizmoSystem::Select(renderable);
-		}
-	}
-	ImGui::End();
+	//ImGui::Begin("Entity List");
+	//ImGui::SetWindowPos({ 0, 20 });
+	//ImGui::SetWindowSize({ 200, 780 });
+	//for (auto renderable : view)
+	//{
+	//	Name name = view.get<Name>(renderable);
+	//	if (ImGui::MenuItem(name.nameString.c_str()))
+	//	{
+	//		GizmoSystem::DeselectAll();
+	//		Transform& newSelected = view.get<Transform>(renderable);
+	//		GizmoSystem::Select(renderable);
+	//	}
+	//}
+	//ImGui::End();
 
-	ImGui::Begin("Inspector");
-	ImGui::SetWindowPos({ 600, 20 });
-	ImGui::SetWindowSize({ 200, 780 });
-	entt::entity selected = GizmoSystem::GetSelectedEntity();
-	Transform* t = registry.try_get<Transform>(selected);
-	if (t)
-	{
-		ComponentGUI(*t);
-	}
-	Renderer* r = registry.try_get<Renderer>(selected);
-	if (r)
-	{
-		ComponentGUI(*r);
-	}
-	Rotator* rot = registry.try_get<Rotator>(selected);
-	if (rot)
-	{
-		ComponentGUI(*rot);
-	}
-	if (ImGui::BeginMenu("+"))
-	{
-		if (ImGui::MenuItem("Rotator"))
-		{
-			if (!registry.has<Rotator>(selected))
-			{
-				registry.emplace_or_replace<Rotator>(selected);
-			}
-		}
-		ImGui::EndMenu();
-	}
-	ImGui::End();
+	//ImGui::Begin("Inspector");
+	//ImGui::SetWindowPos({ 600, 20 });
+	//ImGui::SetWindowSize({ 200, 780 });
+	//entt::entity selected = GizmoSystem::GetSelectedEntity();
+	//Transform* t = registry.try_get<Transform>(selected);
+	//if (t)
+	//{
+	//	ComponentGUI(*t);
+	//}
+	//Renderer* r = registry.try_get<Renderer>(selected);
+	//if (r)
+	//{
+	//	ComponentGUI(*r);
+	//}
+	//Rotator* rot = registry.try_get<Rotator>(selected);
+	//if (rot)
+	//{
+	//	ComponentGUI(*rot);
+	//}
+	//if (ImGui::BeginMenu("+"))
+	//{
+	//	if (ImGui::MenuItem("Rotator"))
+	//	{
+	//		if (!registry.has<Rotator>(selected))
+	//		{
+	//			registry.emplace_or_replace<Rotator>(selected);
+	//		}
+	//	}
+	//	ImGui::EndMenu();
+	//}
+	//ImGui::End();
 
 	auto rotatorView = registry.view<Transform, Rotator>();
 	for (auto rotEntity : rotatorView)
@@ -414,9 +415,9 @@ void Loop()
 	}
 	auto transformView = registry.view<Transform>();
 	//camera.transform = transform;
-	GizmoSystem::DrawGizmo(camera, transformView);
-	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	//GizmoSystem::DrawGizmo(camera, transformView);
+	//ImGui::Render();
+	//ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	graph->_SwapBuffers();
 }
 
@@ -442,10 +443,10 @@ int main(int argc, char* argv[])
 	glm::vec2 prevCursorPos{-1, -1}, currentCursorPos;
 
 	// (Try to) Setup IMGUI	
-	IMGUI_CHECKVERSION();
+	/*IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
-	ImGui::StyleColorsDark();
+	ImGui::StyleColorsDark();*/
 
 
 #ifdef _WIN64
@@ -471,7 +472,7 @@ int main(int argc, char* argv[])
 	//renderer.LoadMesh(mesh.GetRawVertices());
 	LoadMesh(renderer, mesh);
 
-	GizmoSystem::Select(entity);
+	//GizmoSystem::Select(entity);
 
 
 	plat->GetInputSystem()->RegisterRightMouseFunction([]()
@@ -523,9 +524,9 @@ int main(int argc, char* argv[])
 	plat->GetInputSystem()->RegisterKeyPressFunction('e', MoveCamera(glm::vec3(0, 1 * camMoveSpeed, 0)));
 
 	// gizmo keyboard controls
-	plat->GetInputSystem()->RegisterKeyPressFunction('r', []() {GizmoSystem::op = ImGuizmo::SCALE; });
-	plat->GetInputSystem()->RegisterKeyPressFunction('e', []() {GizmoSystem::op = ImGuizmo::ROTATE; });
-	plat->GetInputSystem()->RegisterKeyPressFunction('w', []() {GizmoSystem::op = ImGuizmo::TRANSLATE; });
+	//plat->GetInputSystem()->RegisterKeyPressFunction('r', []() {GizmoSystem::op = ImGuizmo::SCALE; });
+	//plat->GetInputSystem()->RegisterKeyPressFunction('e', []() {GizmoSystem::op = ImGuizmo::ROTATE; });
+	//plat->GetInputSystem()->RegisterKeyPressFunction('w', []() {GizmoSystem::op = ImGuizmo::TRANSLATE; });
 
 	//plat->GetInputSystem()->RegisterKeyPressFunction('b', []() {Serialize(); });
 	//plat->GetInputSystem()->RegisterKeyPressFunction('m', []() {Deserialize(); });
@@ -544,6 +545,6 @@ int main(int argc, char* argv[])
 	delete window;
 	delete plat;
 	delete graph;
-	ImGui::DestroyContext();
+	//ImGui::DestroyContext();
 	return 0;
 }
