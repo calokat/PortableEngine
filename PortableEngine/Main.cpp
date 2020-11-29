e51dd96a065ce876dcab9f153c866bd27e5e4727
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-11-29 10:18:14 -0500

    Main.cpp: Created InitializeAABB()

--- PortableEngine/Main.cpp
+++ PortableEngine/Main.cpp
@@ -224,6 +224,42 @@ void MakeRayFromCamera()
 }
 
 
+void InitializeAABB(AABB& bb, const Mesh& m, const Transform& t)
+{
+	glm::vec3 min(FLT_MAX);
+	glm::vec3 max(FLT_MIN);
+	for (std::vector<Vertex>::const_iterator it = m.rawVertices.begin(); it != m.rawVertices.end(); ++it)
+	{
+		if (it->Position.x < min.x)
+		{
+			min.x = it->Position.x;
+		}
+		if (it->Position.y < min.y)
+		{
+			min.y = it->Position.y;
+		}
+		if (it->Position.z < min.z)
+		{
+			min.z = it->Position.z;
+		}
+		if (it->Position.x > max.x)
+		{
+			max.x = it->Position.x;
+		}
+		if (it->Position.y > max.y)
+		{
+			max.y = it->Position.y;
+		}
+		if (it->Position.z > max.z)
+		{
+			max.z = it->Position.z;
+		}
+	}
+	max += t.position;
+	min += t.position;
+	bb.min = min;
+	bb.max = max;
+}
 //void RandomizeVertexColors()
 //{
 //	auto rcView = registry.view<RandomColor, Mesh, Renderer, Transform>();
