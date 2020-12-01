0ac400cd80a911b6a4494a9046e5108d8fb600d5
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-11-30 23:25:30 -0500

    GizmoSystem: op field is now private. There is no longer a need to expose it now that updating the gizmo is done within the GizmoSystem class

--- PortableEngine/GizmoSystem.h
+++ PortableEngine/GizmoSystem.h
@@ -11,12 +11,12 @@ class GizmoSystem
 private:
 	// TODO: Allow for multiple things to be selected
 	static std::vector<entt::entity> selected;
+	static ImGuizmo::OPERATION op;
 public:
 	static void DrawGizmo(Camera camera, entt::basic_view<entt::entity, entt::exclude_t<>, Transform> transformView);
 	static void Select(entt::entity toSelect);
 	static void Deselect(entt::entity toDeselect);
 	static void DeselectAll();
-	static ImGuizmo::OPERATION op;
 	static entt::entity GetSelectedEntity();
 	static void UpdateGizmo(IInputSystem* inputSystem);
 };
