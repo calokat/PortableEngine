1e7af1c90bfafb560faff5e46e0a1559d7bc6972
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-12-22 13:58:08 -0500

    GLRenderer inherits from IRenderer

--- PortableEngine/GLRenderer.h
+++ PortableEngine/GLRenderer.h
@@ -6,12 +6,13 @@
 #include <string>
 #include <vector>
 #include "Vertex.h"
+#include "IRenderer.h"
 // source: https://github.com/IGME-RIT/Basic-OpenGL-with-GLFW-Assimp/blob/master/OpenGLObjectLoading/mesh.cpp
 #define SetupAttribute(index, size, type, structure, element) \
 	glVertexAttribPointer(index, size, type, 0, sizeof(structure), (void*)offsetof(structure, element)); \
 
 
-class GLRenderer
+class GLRenderer : public IRenderer
 {
 public:
 	Shader vertex;
