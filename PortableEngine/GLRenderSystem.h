f791eddb5e7665d782b221d7444223da6d38feb3
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-12-22 14:54:38 -0500

    Created GLRenderSystem namespace

--- PortableEngine/GLRenderSystem.h
+++ PortableEngine/GLRenderSystem.h
@@ -4,12 +4,15 @@
 #include "GL/glew.h"
 #include "Camera.h"
 
-// source: https://github.com/IGME-RIT/Basic-OpenGL-with-GLFW-Assimp/blob/master/OpenGLObjectLoading/mesh.cpp
+namespace GLRenderSystem 
+{
+	// source: https://github.com/IGME-RIT/Basic-OpenGL-with-GLFW-Assimp/blob/master/OpenGLObjectLoading/mesh.cpp
 #define SetupAttribute(index, size, type, structure, element) \
 	glVertexAttribPointer(index, size, type, 0, sizeof(structure), (void*)offsetof(structure, element)); \
-
-void Load(GLRenderer& renderer, Camera camera);
-void LoadMesh(GLRenderer& renderer, Mesh& mesh);
-void Draw(GLRenderer& renderer);
-void UpdateRenderer(GLRenderer& renderer, Transform meshTransform, Camera camera);
-void DrawGizmo(Camera camera);
+	
+	void Load(GLRenderer& renderer, Camera camera);
+	void LoadMesh(GLRenderer& renderer, Mesh& mesh);
+	void Draw(GLRenderer& renderer);
+	void UpdateRenderer(GLRenderer& renderer, Transform meshTransform, Camera camera);
+	void DrawGizmo(Camera camera);
+}
