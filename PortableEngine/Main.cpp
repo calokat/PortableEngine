dc5ef7d69b906c1c1500b71cc90f3c77fa331518
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-12-25 14:23:22 -0500

    Deleted obsolete right mouse input function

--- PortableEngine/Main.cpp
+++ PortableEngine/Main.cpp
@@ -647,39 +647,6 @@ int main(int argc, char* argv[])
 	//LoadMesh(renderer2, mesh2, ((DirectXAPI*)(graph))->device.Get());
 
 
-	plat->GetInputSystem()->RegisterRightMouseFunction([]()
-	{
-		auto camView = registry.view<Camera>();
-		auto [camera, camTransform] = registry.get<Camera, Transform>(camView[0]);
-		glm::vec2 delta = plat->GetInputSystem()->GetCursorPosition() - plat->GetInputSystem()->GetPreviousCursorPosition();
-		float camRotX = camTransform.rotation.x;
-		bool tooFarUp = camRotX > 3.f / 2;
-		bool tooFarDown = camRotX < -3.f / 2;
-		// checks to see if camera is in danger of gimbal lock
-		if (tooFarUp || tooFarDown)
-		{
-			float newCamRotX;
-			// we allow the input to rotate the camera on the x axis
-			// if it is "too far down" and going up or "too far up" 
-			// and going down. Otherwise zero out the x axis input
-			if ((tooFarUp && delta.y < 0) || (tooFarDown && delta.y > 0))
-			{
-				newCamRotX = delta.y;
-			}
-			else
-			{
-				newCamRotX = 0;
-			}
-			//cam.GetTransform()->Rotate(glm::vec3(newCamRotX * .005f, -delta.x * .005f, 0));
-			TransformSystem::Rotate(glm::vec3(newCamRotX * .005f, delta.x * .005f, 0), &camTransform);
-		}
-		else
-		{
-			//cam.GetTransform()->Rotate(glm::vec3(delta.y * .005f, -delta.x * .005f, 0));
-			TransformSystem::Rotate(glm::vec3(delta.y * .005f, delta.x * .005f, 0), &camTransform);
-		}
-	});
-
 	auto MoveCamera = [](glm::vec3 dir) {
 		return[dir]() {
 			auto camView = registry.view<Camera>();
