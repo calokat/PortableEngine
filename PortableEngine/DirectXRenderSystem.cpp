8587b1a83fb40d591fbba8efa5a26234bd57b2d2
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-12-26 13:44:02 -0500

    Converted DirectXRenderSystem from a namespace to a class derived from IRenderSystem

--- PortableEngine/DirectXRenderSystem.cpp
+++ PortableEngine/DirectXRenderSystem.cpp
@@ -1,82 +1,95 @@
 #include "DirectXRenderSystem.h"
 
-namespace DirectXRenderSystem
+
+IRenderer& DirectXRenderSystem::CreateRenderer(entt::registry& reg, entt::entity& e)
+{
+	IRenderer& rendererRef = reg.emplace<DirectXRenderer>(e);
+	return rendererRef;
+}
+
+void DirectXRenderSystem::Load(IRenderer* renderer, Camera& camera)
+{
+	DirectXRenderer* dxRenderer = (DirectXRenderer*)renderer;
+	dxRenderer->vertexShader = new SimpleVertexShader(device, context, L"VertexShader.cso");
+	dxRenderer->pixelShader = new SimplePixelShader(device, context, L"PixelShader.cso");
+	dxRenderer->vertexShader->SetMatrix4x4("viewMatrix", camera.view);
+	dxRenderer->vertexShader->SetMatrix4x4("projectionMatrix", camera.projection);
+	dxRenderer->vertexShader->SetMatrix4x4("worldMatrix", glm::mat4(1.0f));
+	dxRenderer->vertexShader->CopyAllBufferData();
+}
+
+void DirectXRenderSystem::LoadMesh(IRenderer* renderer, Mesh& mesh)
+{
+	DirectXRenderer* dxRenderer = (DirectXRenderer*)renderer;
+	dxRenderer->numVertices = mesh.rawVertices.size();
+	dxRenderer->numIndices = mesh.numIndices;
+	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertex) * dxRenderer->numVertices;       // 3 = number of vertices in the buffer
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER; // Tells DirectX this is a vertex buffer
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	vbd.StructureByteStride = 0;
+
+	D3D11_SUBRESOURCE_DATA initialVertexData;
+	initialVertexData.pSysMem = mesh.rawVertices.data();
+
+	device->CreateBuffer(&vbd, &initialVertexData, dxRenderer->vertexBuffer.GetAddressOf());
+
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(int) * dxRenderer->numIndices;         // 3 = number of indices in the buffer
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER; // Tells DirectX this is an index buffer
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+	ibd.StructureByteStride = 0;
+
+	D3D11_SUBRESOURCE_DATA initialIndexData;
+	initialIndexData.pSysMem = mesh.rawIndices.data();
+
+	device->CreateBuffer(&ibd, &initialIndexData, dxRenderer->indexBuffer.GetAddressOf());
+}
+
+void DirectXRenderSystem::Draw(IRenderer* renderer)
 {
 	UINT stride = sizeof(Vertex);
 	UINT offset = 0;
 
-	void Load(DirectXRenderer& renderer, Camera camera, DirectXAPI* dxApi, WindowsPlatform* winPlat)
-	{
-		renderer.vertexShader = new SimpleVertexShader(dxApi->device.Get(), dxApi->context.Get(), L"VertexShader.cso");
-		renderer.pixelShader = new SimplePixelShader(dxApi->device.Get(), dxApi->context.Get(), L"PixelShader.cso");
-		renderer.vertexShader->SetMatrix4x4("viewMatrix", camera.view);
-		renderer.vertexShader->SetMatrix4x4("projectionMatrix", camera.projection);
-		renderer.vertexShader->SetMatrix4x4("worldMatrix", glm::mat4(1.0f));
-		renderer.vertexShader->CopyAllBufferData();
-	}
-
-	void LoadMesh(DirectXRenderer& renderer, Mesh& mesh, ID3D11Device* device)
-	{
-		renderer.numVertices = mesh.rawVertices.size();
-		renderer.numIndices = mesh.numIndices;
-		D3D11_BUFFER_DESC vbd;
-		vbd.Usage = D3D11_USAGE_IMMUTABLE;
-		vbd.ByteWidth = sizeof(Vertex) * renderer.numVertices;       // 3 = number of vertices in the buffer
-		vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER; // Tells DirectX this is a vertex buffer
-		vbd.CPUAccessFlags = 0;
-		vbd.MiscFlags = 0;
-		vbd.StructureByteStride = 0;
-
-		D3D11_SUBRESOURCE_DATA initialVertexData;
-		initialVertexData.pSysMem = mesh.rawVertices.data();
-
-		device->CreateBuffer(&vbd, &initialVertexData, renderer.vertexBuffer.GetAddressOf());
-
-		D3D11_BUFFER_DESC ibd;
-		ibd.Usage = D3D11_USAGE_IMMUTABLE;
-		ibd.ByteWidth = sizeof(int) * renderer.numIndices;         // 3 = number of indices in the buffer
-		ibd.BindFlags = D3D11_BIND_INDEX_BUFFER; // Tells DirectX this is an index buffer
-		ibd.CPUAccessFlags = 0;
-		ibd.MiscFlags = 0;
-		ibd.StructureByteStride = 0;
-
-		D3D11_SUBRESOURCE_DATA initialIndexData;
-		initialIndexData.pSysMem = mesh.rawIndices.data();
-
-		device->CreateBuffer(&ibd, &initialIndexData, renderer.indexBuffer.GetAddressOf());
-	}
-
-	void Draw(DirectXRenderer& renderer, ID3D11DeviceContext* context)
-	{
-		context->IASetVertexBuffers(0, 1, renderer.vertexBuffer.GetAddressOf(), &stride, &offset);
-		context->IASetIndexBuffer(renderer.indexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
-		renderer.vertexShader->SetShader();
-		renderer.pixelShader->SetShader();
-		context->DrawIndexed(
-			renderer.numIndices,     // The number of indices to use (we could draw a subset if we wanted)
-			0,     // Offset to the first index we want to use
-			0);    // Offset to add to each index when looking up vertices
-
-	}
-
-	void DrawWireframe(DirectXRenderer& renderer, ID3D11DeviceContext* context)
-	{
-		glm::vec4 oldRendererColor = renderer.vertexColor;
-		renderer.vertexShader->SetFloat4("colorTint", glm::vec4(1, 1, 1, 1));
-		renderer.vertexShader->CopyAllBufferData();
-		context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
-		Draw(renderer, context);
-		context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-		renderer.vertexShader->SetFloat4("colorTint", oldRendererColor);
-	}
-
-	void UpdateRenderer(DirectXRenderer& renderer, Transform meshTransform, Camera camera)
-	{
-		renderer.vertexShader->SetMatrix4x4("viewMatrix", camera.view);
-		renderer.vertexShader->SetMatrix4x4("projectionMatrix", camera.projection);
-		renderer.vertexShader->SetMatrix4x4("worldMatrix", meshTransform.worldMatrix);
-		renderer.vertexShader->SetFloat4("colorTint", renderer.vertexColor);
-		renderer.vertexShader->CopyAllBufferData();
-	}
+	DirectXRenderer* dxRenderer = (DirectXRenderer*)renderer;
+	context->IASetVertexBuffers(0, 1, dxRenderer->vertexBuffer.GetAddressOf(), &stride, &offset);
+	context->IASetIndexBuffer(dxRenderer->indexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
+	dxRenderer->vertexShader->SetShader();
+	dxRenderer->pixelShader->SetShader();
+	context->DrawIndexed(
+		dxRenderer->numIndices,     // The number of indices to use (we could draw a subset if we wanted)
+		0,     // Offset to the first index we want to use
+		0);    // Offset to add to each index when looking up vertices
 
 }
+
+void DirectXRenderSystem::DrawWireframe(IRenderer* renderer)
+{
+	DirectXRenderer* dxRenderer = (DirectXRenderer*)renderer;
+	glm::vec4 oldRendererColor = dxRenderer->vertexColor;
+	dxRenderer->vertexShader->SetFloat4("colorTint", glm::vec4(1, 1, 1, 1));
+	dxRenderer->vertexShader->CopyAllBufferData();
+	context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
+	Draw(dxRenderer);
+	context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	dxRenderer->vertexShader->SetFloat4("colorTint", oldRendererColor);
+}
+
+void DirectXRenderSystem::UpdateRenderer(IRenderer* renderer, Transform meshTransform, Camera camera)
+{
+	DirectXRenderer* dxRenderer = (DirectXRenderer*)renderer;
+	dxRenderer->vertexShader->SetMatrix4x4("viewMatrix", camera.view);
+	dxRenderer->vertexShader->SetMatrix4x4("projectionMatrix", camera.projection);
+	dxRenderer->vertexShader->SetMatrix4x4("worldMatrix", meshTransform.worldMatrix);
+	dxRenderer->vertexShader->SetFloat4("colorTint", dxRenderer->vertexColor);
+	dxRenderer->vertexShader->CopyAllBufferData();
+}
+
+DirectXRenderSystem::DirectXRenderSystem(ID3D11Device* dev, ID3D11DeviceContext* ctx) : device(dev), context(ctx)
+{
+}
+
--- PortableEngine/DirectXRenderSystem.h
+++ PortableEngine/DirectXRenderSystem.h
@@ -3,13 +3,20 @@
 #include "Mesh.h"
 #include "Camera.h"
 #include "DirectXAPI.h"
+#include "IRenderSystem.h"
 
-namespace DirectXRenderSystem
+class DirectXRenderSystem : public IRenderSystem
 {
-	void Load(DirectXRenderer& renderer, Camera camera, DirectXAPI* dxApi, WindowsPlatform* winPlat);
-	void LoadMesh(DirectXRenderer& renderer, Mesh& mesh, ID3D11Device* device);
-	void Draw(DirectXRenderer& DirectXRenderer, ID3D11DeviceContext* context);
-	void DrawWireframe(DirectXRenderer& DirectXRenderer, ID3D11DeviceContext* context);
-	void UpdateRenderer(DirectXRenderer& renderer, Transform meshTransform, Camera camera);
-}
+public:
+	IRenderer& CreateRenderer(entt::registry& reg, entt::entity& e);
+	void Load(IRenderer* renderer, Camera& camera);
+	void LoadMesh(IRenderer* renderer, Mesh& mesh);
+	void Draw(IRenderer* DirectXRenderer);
+	void DrawWireframe(IRenderer* DirectXRenderer);
+	void UpdateRenderer(IRenderer* renderer, Transform meshTransform, Camera camera);
+	DirectXRenderSystem(ID3D11Device* dev, ID3D11DeviceContext* ctx);
+private:
+	ID3D11Device* device;
+	ID3D11DeviceContext* context;
+};
 //void DrawGizmo(Camera camera);
