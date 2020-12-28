67b3171cb495e748dbcc066d0a658bebb0cd050d
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-12-28 11:22:45 -0500

    Deleted some of the old code for capturing input on the web

--- PortableEngine/EmscriptenInputSystem.h
+++ PortableEngine/EmscriptenInputSystem.h
@@ -9,8 +9,6 @@ class EmscriptenInputSystem :
 public:
 	EmscriptenInputSystem();
 	void GetKeyPressed();
-	void RegisterKeyPressFunction(char, std::function<void()>);
-	void RegisterRightMouseFunction(std::function<void()>);
 	glm::vec2 GetCursorPosition();
 	glm::vec2 GetPreviousCursorPosition();
 	bool IsKeyPressed(KeyboardCode kc);
@@ -22,9 +20,5 @@ private:
 	static EM_BOOL MouseClickCallback(int eventType, const EmscriptenMouseEvent* mouseEvent, void* userData);
 	static EM_BOOL MouseUpCallback(int eventType, const EmscriptenMouseEvent* mouseEvent, void* userData);
 	static EM_BOOL KeyDownCallback(int eventType, const EmscriptenKeyboardEvent* kbEvent, void* userData);
-	static glm::vec2 cursorPos;
-	static glm::vec2 prevCursorPos;
-	static std::function<void()> rightClickFunction;
-	static std::map<char, std::function<void()>> keyToFunction;
 };
 #endif
