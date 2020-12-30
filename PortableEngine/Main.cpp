c8db1741e4a3cc8c0265e52107b2e67c3e34e3cd
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-12-30 11:29:59 -0500

    An additional renderSystem->LoadMesh() call is required after all. Without it, wireframes are renderered incorrectly

--- PortableEngine/Main.cpp
+++ PortableEngine/Main.cpp
@@ -266,6 +266,7 @@ void DrawIteration(Camera& camera, entt::entity selected)
 	{
 		Mesh& selectedMesh = registry.get<Mesh>(selected);
 		T& r = renderableView.template get<T>(selected);
+		renderSystem->LoadMesh(&r, selectedMesh);
 		renderSystem->DrawWireframe(&r);
 	}
 }
