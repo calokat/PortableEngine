3d51539cc630c42d865c1b3166c9b4b69243a519
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-12-31 14:12:32 -0500

    Added ImageSystem::LoadImage()

--- PortableEngine/ImageSystem.cpp
+++ PortableEngine/ImageSystem.cpp
@@ -1,1 +1,7 @@
 #include "ImageSystem.h"
+
+bool ImageSystem::LoadImage(const char* path, PEImage& img)
+{
+	img.data = stbi_load(path, &img.width, &img.height, &img.numChannels, 0);
+	return img.data != nullptr;
+}
--- PortableEngine/ImageSystem.h
+++ PortableEngine/ImageSystem.h
@@ -1,5 +1,8 @@
 #pragma once
+#include "PEImage.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 class ImageSystem
 {
+	bool LoadImage(const char* path, PEImage& img);
 };
-
