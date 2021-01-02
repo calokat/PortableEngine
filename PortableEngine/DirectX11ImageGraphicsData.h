c48ff003550c2dce00afa8a541ba8a867bfdc26c
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-02 13:06:41 -0500

    Added IImageGraphicsData as well as its subclasses

---
+++ PortableEngine/DirectX11ImageGraphicsData.h
@@ -0,0 +1,9 @@
+#ifdef _WIN64
+#pragma once
+#include <d3d11.h>
+#include "IImageGraphicsData.h"
+struct DirectX11ImageGraphicsData : public IImageGraphicsData
+{
+	ID3D11ShaderResourceView* srv;
+};
+#endif
---
+++ PortableEngine/OpenGLImageGraphicsData.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "IImageGraphicsData.h"
+#include <GL/glew.h>
+struct OpenGLImageGraphicsData : public IImageGraphicsData
+{
+	GLuint texture;
+};
