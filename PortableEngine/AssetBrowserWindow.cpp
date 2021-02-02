39ac7dd0497a8615f4918c629afc4d1cc7a8d0db
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-02-02 13:40:19 -0500

    Created AssetBrowserWindow

---
+++ PortableEngine/AssetBrowserWindow.cpp
@@ -0,0 +1,23 @@
+#include "AssetBrowserWindow.h"
+#include <Windows.h>
+#include "OpenGLImageGraphicsData.h"
+#include <imgui.h>
+void AssetBrowserWindow::Render(Thumbnail& assetThumbnail)
+{
+	const char* projectPath = "C:/Users/Caleb/Documents/Github/PE_Projects/demo/*";
+	ImGui::Begin("Asset Browser");
+	ImGui::SetWindowPos({ 200, 400 });
+	ImGui::SetWindowSize({ 400, 200 });
+	WIN32_FIND_DATA findResult;
+	HANDLE fileHandle = FindFirstFile(projectPath, &findResult);
+	FindNextFile(fileHandle, &findResult);
+	while (FindNextFile(fileHandle, &findResult))
+	{
+		ImGui::BeginGroup();
+		ImGui::Image((void*)((OpenGLImageGraphicsData*)(assetThumbnail.assetImage.imageGraphicsData))->texture, ImVec2(50, 50));
+		ImGui::Text("%s", findResult.cFileName);
+		ImGui::EndGroup();
+		ImGui::SameLine();
+	}
+	ImGui::End();
+}
---
+++ PortableEngine/AssetBrowserWindow.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "misc_components.h"
+class AssetBrowserWindow
+{
+public:
+	void Render(Thumbnail& assetThumbnail);
+};
+
