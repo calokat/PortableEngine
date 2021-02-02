85a8e836c29a73fa38b95ba4aad304029de00035
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-02-02 14:25:47 -0500

    IAssetManager interface methods are now public

--- PortableEngine/IAssetManager.h
+++ PortableEngine/IAssetManager.h
@@ -2,6 +2,7 @@
 #include <string>
 class IAssetManager
 {
+public:
 	virtual std::string GetAssetPath(std::string relativePath) = 0;
 	virtual std::wstring GetAssetPath_Wide(std::wstring relativePath) = 0;
 };
