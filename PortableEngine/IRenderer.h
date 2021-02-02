e88cbd99adedd10c006ea1be9f509e13007c4184
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-02-02 11:52:03 -0500

    Added move assignment operator to IRenderer so I could assign to IRenderer variables after declaration

--- PortableEngine/IRenderer.h
+++ PortableEngine/IRenderer.h
@@ -4,5 +4,13 @@ class IRenderer
 {
 public:
 	virtual ~IRenderer() {};
+	virtual IRenderer& operator=(IRenderer&& other) noexcept 
+	{
+		if (this != &other)
+		{
+			this->diffuseTexture = std::move(other.diffuseTexture);
+		}
+		return *this;
+	};
 	PEImage diffuseTexture;
 };
