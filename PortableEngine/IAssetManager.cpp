7c4323801c4afb1d328719d8b2014b2884ebb81c
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-02-03 12:51:25 -0500

    Added LoadDefaultThumbnails(), and getters for the asset and directory thumbnails

---
+++ PortableEngine/IAssetManager.cpp
@@ -0,0 +1,22 @@
+#include "IAssetManager.h"
+#include "ImageSystem.h"
+void IAssetManager::LoadDefaultThumbnails(IRenderSystem* renderSystem)
+{
+	assetThumbnail = PEImage(GetAssetPath("../../Assets/Images/asset.png"));
+	ImageSystem::CreateImage(assetThumbnail);
+	renderSystem->LoadTexture(assetThumbnail);
+
+	directoryThumbnail = PEImage(GetAssetPath("../../Assets/Images/directory.png"));
+	ImageSystem::CreateImage(directoryThumbnail);
+	renderSystem->LoadTexture(directoryThumbnail);
+}
+
+const PEImage& IAssetManager::GetDefaultAssetImage()
+{
+	return assetThumbnail;
+}
+
+const PEImage& IAssetManager::GetDirectoryImage()
+{
+	return directoryThumbnail;
+}
--- PortableEngine/IAssetManager.h
+++ PortableEngine/IAssetManager.h
@@ -1,8 +1,16 @@
 #pragma once
 #include <string>
+#include "IRenderSystem.h"
+
 class IAssetManager
 {
 public:
 	virtual std::string GetAssetPath(std::string relativePath) = 0;
 	virtual std::wstring GetAssetPath_Wide(std::wstring relativePath) = 0;
+	void LoadDefaultThumbnails(IRenderSystem* renderSystem);
+	const PEImage& GetDefaultAssetImage();
+	const PEImage& GetDirectoryImage();
+protected:
+	PEImage assetThumbnail;
+	PEImage directoryThumbnail;
 };
