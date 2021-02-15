be977f5f0726cf05f8961b755433ac0909bfc64d
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-02-15 09:14:14 -0500

    OpenGLESContext: Now using an SDL implementation

--- PortableEngine/OpenGLESContext.cpp
+++ PortableEngine/OpenGLESContext.cpp
@@ -1,97 +1,115 @@
 #ifdef __EMSCRIPTEN__
 #include "OpenGLESContext.h"
 #include <iostream>
+#include <examples/imgui_impl_sdl.h>
+#include <imgui.h>
 
-	ESContext esContext;
+
+	// ESContext esContext;
 
 int OpenGLESContext::GetContext(GameWindow* window)
 {
-	//esInitContext(&esContext);
-	//GLboolean iffy = esCreateWindow ( &esContext, "Hello Triangle", 320, 240, ES_WINDOW_RGB | ES_WINDOW_DEPTH );
-	//if (iffy)
+	//GLuint flags = ES_WINDOW_RGB | ES_WINDOW_DEPTH;
+
+	//EGLint attribList[] =
 	//{
-	//	printf("WHY\n");
-	//}
-	//else
+	//	EGL_RED_SIZE,       5,
+	//	EGL_GREEN_SIZE,     6,
+	//	EGL_BLUE_SIZE,      5,
+	//	EGL_ALPHA_SIZE,     (flags & ES_WINDOW_ALPHA) ? 8 : EGL_DONT_CARE,
+	//	EGL_DEPTH_SIZE,     (flags & ES_WINDOW_DEPTH) ? 8 : EGL_DONT_CARE,
+	//	EGL_STENCIL_SIZE,   (flags & ES_WINDOW_STENCIL) ? 8 : EGL_DONT_CARE,
+	//	EGL_SAMPLE_BUFFERS, (flags & ES_WINDOW_MULTISAMPLE) ? 1 : 0,
+	//	EGL_NONE
+	//};
+
+
+	//EGLint numConfigs;
+	//EGLint majorVersion;
+	//EGLint minorVersion;
+	//EGLDisplay display;
+	//EGLContext context;
+	//EGLSurface surface;
+	//EGLConfig config;
+	//EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE, EGL_NONE };
+
+	//// Get Display
+	//display = eglGetDisplay((EGLNativeDisplayType)platform->GetDeviceContext());
+	//if (display == EGL_NO_DISPLAY)
 	//{
-	//	printf("Aha!\n");
+	//	return EGL_FALSE;
 	//}
 
-	GLuint flags = ES_WINDOW_RGB | ES_WINDOW_DEPTH;
+	//if (!eglInitialize(display, &majorVersion, &minorVersion))
+	//{
+	//	return EGL_FALSE;
+	//}
 
-	EGLint attribList[] =
+	//if (!eglGetConfigs(display, NULL, 0, &numConfigs))
+	//{
+	//	return EGL_FALSE;
+	//}
+
+	//if (!eglChooseConfig(display, attribList, &config, 1, &numConfigs))
+	//{
+	//	return EGL_FALSE;
+	//}
+
+	//surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType)platform->GetWindowHandle(), NULL);
+
+	//if (surface == EGL_NO_SURFACE)
+	//{
+	//	return EGL_FALSE;
+	//}
+
+	//context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
+	//if (context == EGL_NO_CONTEXT)
+	//{
+	//	return EGL_FALSE;
+	//}
+
+	//if (!eglMakeCurrent(display, surface, surface, context))
+	//{
+	//	return EGL_FALSE;
+	//}
+
+
+	//
+	//glEnable(GL_DEPTH_TEST);
+	// EmscriptenWebGLContextAttributes attr;
+	//emscripten_webgl_init_context_attributes(&attr);
+	// SDL_GL_SetAttribute(GL_CONTEXT_FLAGS, 0);
+	// SDL_GL_SetAttribute(GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+	// Create window with graphics context
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_DisplayMode current;
+	SDL_GetCurrentDisplayMode(0, &current);
+
+
+	g_GLContext = SDL_GL_CreateContext((SDL_Window*)platform->GetWindowHandle());
+
+	if (!g_GLContext)
 	{
-		EGL_RED_SIZE,       5,
-		EGL_GREEN_SIZE,     6,
-		EGL_BLUE_SIZE,      5,
-		EGL_ALPHA_SIZE,     (flags & ES_WINDOW_ALPHA) ? 8 : EGL_DONT_CARE,
-		EGL_DEPTH_SIZE,     (flags & ES_WINDOW_DEPTH) ? 8 : EGL_DONT_CARE,
-		EGL_STENCIL_SIZE,   (flags & ES_WINDOW_STENCIL) ? 8 : EGL_DONT_CARE,
-		EGL_SAMPLE_BUFFERS, (flags & ES_WINDOW_MULTISAMPLE) ? 1 : 0,
-		EGL_NONE
-	};
-
-
-	EGLint numConfigs;
-	EGLint majorVersion;
-	EGLint minorVersion;
-	EGLDisplay display;
-	EGLContext context;
-	EGLSurface surface;
-	EGLConfig config;
-	EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE, EGL_NONE };
-
-	// Get Display
-	display = eglGetDisplay((EGLNativeDisplayType)platform->GetDeviceContext());
-	if (display == EGL_NO_DISPLAY)
-	{
-		return EGL_FALSE;
+		fprintf(stderr, "Failed to initialize WebGL context!\n");
+		return 1;
 	}
-
-	if (!eglInitialize(display, &majorVersion, &minorVersion))
-	{
-		return EGL_FALSE;
-	}
-
-	if (!eglGetConfigs(display, NULL, 0, &numConfigs))
-	{
-		return EGL_FALSE;
-	}
-
-	if (!eglChooseConfig(display, attribList, &config, 1, &numConfigs))
-	{
-		return EGL_FALSE;
-	}
-
-	surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType)platform->GetWindowHandle(), NULL);
-
-	if (surface == EGL_NO_SURFACE)
-	{
-		return EGL_FALSE;
-	}
-
-	context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
-	if (context == EGL_NO_CONTEXT)
-	{
-		return EGL_FALSE;
-	}
-
-	if (!eglMakeCurrent(display, surface, surface, context))
-	{
-		return EGL_FALSE;
-	}
-
-
-	
+	ImGui_ImplSDL2_InitForOpenGL((SDL_Window*)platform->GetWindowHandle(), g_GLContext);
+	SDL_GL_SetSwapInterval(1);
 	glEnable(GL_DEPTH_TEST);
-	 EmscriptenWebGLContextAttributes attr;
-	emscripten_webgl_init_context_attributes(&attr);
+
 	return 0;
 }
 
 void OpenGLESContext::_SwapBuffers(int winHandle)
 {
-	eglSwapBuffers(eglDisplay, eglSurface);
+	//eglSwapBuffers(eglDisplay, eglSurface);
+	SDL_GL_MakeCurrent((SDL_Window*)platform->GetWindowHandle(), g_GLContext);
+	SDL_GL_SwapWindow((SDL_Window*)platform->GetWindowHandle());
 }
 OpenGLESContext::OpenGLESContext(IPlatform* plat) : platform(plat)
 {
--- PortableEngine/OpenGLESContext.h
+++ PortableEngine/OpenGLESContext.h
@@ -1,11 +1,14 @@
 #ifdef __EMSCRIPTEN__
 #pragma once
-#include "esUtil.h"
+//#include "esUtil.h"
 #include "GameWindow.h"
 #include "IOpenGLContext.h"
 #include "IPlatform.h"
 #include <emscripten.h>
 #include <emscripten/html5.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengles2.h>
+
 
 class OpenGLESContext : public IOpenGLContext
 {
@@ -15,7 +18,9 @@ public:
 	OpenGLESContext(IPlatform* plat);
 private:
 	IPlatform* platform;
-	EGLDisplay eglDisplay;
-	EGLSurface eglSurface;
+	// EGLDisplay eglDisplay;
+	// EGLSurface eglSurface;
+	SDL_GLContext g_GLContext = NULL;
+
 };
 #endif
