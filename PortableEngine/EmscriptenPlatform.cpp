612da6bffd3bf1b27d679a4b46bd66012e4ff89f
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-02-15 09:11:44 -0500

    EmscriptenPlatform: Replaced custom X11 and EGL code with SDL implementation

--- PortableEngine/EmscriptenPlatform.cpp
+++ PortableEngine/EmscriptenPlatform.cpp
@@ -2,14 +2,21 @@
 #include "EmscriptenPlatform.h"
 #include "EmscriptenInputSystem.h"
 #include <GLES3/gl3.h>
-#include "imgui_impl_emscripten.h"
+//#include "imgui_impl_emscripten.h"
 #include "EmscriptenAssetManager.h"
+#include <examples/imgui_impl_sdl.h>
+#include <SDL/SDL_opengles2.h>
 
 EmscriptenPlatform::EmscriptenPlatform(GameWindow* win) : window(win)
 {
 	inputSystem = new EmscriptenInputSystem();
 	assetManager = new EmscriptenAssetManager();
-	ImGui_ImplEmscripten_Init(window, inputSystem);
+	//ImGui_ImplEmscripten_Init(window, inputSystem);
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+	{
+		printf("Error: %s\n", SDL_GetError());
+		return;
+	}
 }
 
 EmscriptenPlatform::~EmscriptenPlatform()
@@ -20,65 +27,72 @@ EmscriptenPlatform::~EmscriptenPlatform()
 
 int EmscriptenPlatform::InitWindow()
 {
-	Window root;
-	XSetWindowAttributes swa;
-	XSetWindowAttributes  xattr;
-	Atom wm_state;
-	XWMHints hints;
-	XEvent xev;
-	EGLConfig ecfg;
-	EGLint num_config;
-	Window win;
+	//Window root;
+	//XSetWindowAttributes swa;
+	//XSetWindowAttributes  xattr;
+	//Atom wm_state;
+	//XWMHints hints;
+	//XEvent xev;
+	//EGLConfig ecfg;
+	//EGLint num_config;
+	//Window win;
 
-	/*
-	 * X11 native display initialization
-	 */
+	///*
+	// * X11 native display initialization
+	// */
 
-	x_display = XOpenDisplay(NULL);
-	if (x_display == NULL)
-	{
-		return EGL_FALSE;
-	}
+	//x_display = XOpenDisplay(NULL);
+	//if (x_display == NULL)
+	//{
+	//	return EGL_FALSE;
+	//}
 
-	root = DefaultRootWindow(x_display);
+	//root = DefaultRootWindow(x_display);
 
-	swa.event_mask = ExposureMask | PointerMotionMask | KeyPressMask;
-	win = XCreateWindow(
-		x_display, root,
-		0, 0, window->width, window->height, 0,
-		CopyFromParent, InputOutput,
-		CopyFromParent, CWEventMask,
-		&swa);
+	//swa.event_mask = ExposureMask | PointerMotionMask | KeyPressMask;
+	//win = XCreateWindow(
+	//	x_display, root,
+	//	0, 0, window->width, window->height, 0,
+	//	CopyFromParent, InputOutput,
+	//	CopyFromParent, CWEventMask,
+	//	&swa);
 
-	xattr.override_redirect = 0;
-	XChangeWindowAttributes(x_display, win, CWOverrideRedirect, &xattr);
+	//xattr.override_redirect = 0;
+	//XChangeWindowAttributes(x_display, win, CWOverrideRedirect, &xattr);
 
-	hints.input = 1;
-	hints.flags = InputHint;
-	XSetWMHints(x_display, win, &hints);
+	//hints.input = 1;
+	//hints.flags = InputHint;
+	//XSetWMHints(x_display, win, &hints);
 
-	// make the window visible on the screen
-	XMapWindow(x_display, win);
-	XStoreName(x_display, win, "EmscriptenGL");
+	//// make the window visible on the screen
+	//XMapWindow(x_display, win);
+	//XStoreName(x_display, win, "EmscriptenGL");
 
-	// get identifiers for the provided atom name strings
-	wm_state = XInternAtom(x_display, "_NET_WM_STATE", 0);
+	//// get identifiers for the provided atom name strings
+	//wm_state = XInternAtom(x_display, "_NET_WM_STATE", 0);
 
-	memset(&xev, 0, sizeof(xev));
-	xev.type = ClientMessage;
-	xev.xclient.window = win;
-	xev.xclient.message_type = wm_state;
-	xev.xclient.format = 32;
-	xev.xclient.data.l[0] = 1;
-	xev.xclient.data.l[1] = 0;
-	XSendEvent(
-		x_display,
-		DefaultRootWindow(x_display),
-		0,
-		SubstructureNotifyMask,
-		&xev);
+	//memset(&xev, 0, sizeof(xev));
+	//xev.type = ClientMessage;
+	//xev.xclient.window = win;
+	//xev.xclient.message_type = wm_state;
+	//xev.xclient.format = 32;
+	//xev.xclient.data.l[0] = 1;
+	//xev.xclient.data.l[1] = 0;
+	//XSendEvent(
+	//	x_display,
+	//	DefaultRootWindow(x_display),
+	//	0,
+	//	SubstructureNotifyMask,
+	//	&xev);
 
-	hWnd = (EGLNativeWindowType)win;
+	//hWnd = (EGLNativeWindowType)win;
+    SDL_DisplayMode current;
+    SDL_GetCurrentDisplayMode(0, &current);
+
+	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_INPUT_FOCUS);
+	g_Window = SDL_CreateWindow("Dear ImGui Emscripten example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window->width, window->height, window_flags);
+
+	SDL_RaiseWindow(g_Window);
 	return 0;
 }
 
@@ -98,11 +112,11 @@ std::wstring EmscriptenPlatform::GetAssetPath_Wide(std::wstring relativePath)
 }
 void* EmscriptenPlatform::GetWindowHandle()
 {
-	return &hWnd;
+	return g_Window;
 }
 void* EmscriptenPlatform::GetDeviceContext()
 {
-	return x_display;
+	return g_Window;
 }
 IInputSystem* EmscriptenPlatform::GetInputSystem()
 {
@@ -110,7 +124,7 @@ IInputSystem* EmscriptenPlatform::GetInputSystem()
 }
 void EmscriptenPlatform::NewGuiFrame()
 {
-	ImGui_ImplEmscripten_NewFrame();
+	ImGui_ImplSDL2_NewFrame(g_Window);
 }
 
 IAssetManager* EmscriptenPlatform::GetAssetManager()
