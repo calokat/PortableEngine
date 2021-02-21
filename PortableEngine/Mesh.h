3ff430bacc994b2dbee596664fd007e80f454c54
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-02-21 11:55:32 -0500

    Cleaned up Mesh class further

--- PortableEngine/Mesh.h
+++ PortableEngine/Mesh.h
@@ -4,26 +4,13 @@
 #include <string>
 class Mesh
 {
-private:
-	//Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
-	//Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer;
-	//void CreateBuffers(Vertex* vertices, int numVertices, unsigned int* indices, int numIndicesParam, Microsoft::WRL::ComPtr<ID3D11Device> device);
-
 public:
 	Mesh(void);
-	//Mesh(Vertex* vertices, int numVertices, unsigned int* indices, int numIndicesParam, Microsoft::WRL::ComPtr<ID3D11Device> device);
-	//Mesh(const char* fileName, Microsoft::WRL::ComPtr<ID3D11Device> device);
 	Mesh(const char* fileName);
-	//Microsoft::WRL::ComPtr<ID3D11Buffer> GetVertexBuffer();
-	//Microsoft::WRL::ComPtr<ID3D11Buffer> GetIndexBuffer();
 	int numIndices;
 	std::string path;
 	std::vector<Vertex> rawVertices;
 	std::vector<unsigned int> rawIndices;
-	//int GetIndexCount();
-	//void CalculateTangents(Vertex* verts, int numVerts, unsigned int* indices, int numIndices);
-	//std::vector<Vertex>& GetRawVertices();
-	//std::vector<unsigned int> GetRawIndices();
 	// Move assignment operator
 	Mesh& operator=(Mesh&& other);
 	// Move constructor
