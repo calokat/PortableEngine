72b3ba155269fc99593f871340c6cb6077984827
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-03-21 18:33:17 -0400

    Declared methods from openxr_program.h in hello_xr

--- PortableEngine/XRAPI.h
+++ PortableEngine/XRAPI.h
@@ -1,8 +1,17 @@
 #pragma once
 #include "IPlatform.h"
 #include "IGraphicsAPI.h"
+#include "xr_defines.h"
 #include "openxr/openxr.h"
 #include "openxr/openxr_platform.h"
+#include "openxr/xr_linear.h"
+
+struct Swapchain {
+    XrSwapchain handle;
+    int32_t width;
+    int32_t height;
+};
+
 
 class XRAPI
 {
@@ -11,8 +20,32 @@ public:
 private:
 	IPlatform* platform;
 	IGraphicsAPI* graphics;
-	XrInstance m_instance;
 	XrResult Init();
 	XrResult CreateXRInstance();
+	void InitializeXRSystem();
+	void InitializeXRSession();
+	void CreateVisualizedSpaces();
+	void CreateSwapchains();
+    XrInstance m_instance{ XR_NULL_HANDLE };
+    XrSession m_session{ XR_NULL_HANDLE };
+    XrSpace m_appSpace{ XR_NULL_HANDLE };
+    XrFormFactor m_formFactor{ XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY };
+    XrViewConfigurationType m_viewConfigType{ XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO };
+    XrEnvironmentBlendMode m_environmentBlendMode{ XR_ENVIRONMENT_BLEND_MODE_OPAQUE };
+    XrSystemId m_systemId{ XR_NULL_SYSTEM_ID };
+
+    std::vector<XrViewConfigurationView> m_configViews;
+    std::vector<Swapchain> m_swapchains;
+    std::map<XrSwapchain, std::vector<XrSwapchainImageBaseHeader*>> m_swapchainImages;
+    std::vector<XrView> m_views;
+    int64_t m_colorSwapchainFormat{ -1 };
+
+    std::vector<XrSpace> m_visualizedSpaces;
+
+    // Application's current lifecycle state according to the runtime
+    XrSessionState m_sessionState{ XR_SESSION_STATE_UNKNOWN };
+    bool m_sessionRunning{ false };
+
+    XrEventDataBuffer m_eventDataBuffer;
 };
 
