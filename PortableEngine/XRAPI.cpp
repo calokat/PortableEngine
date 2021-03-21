ebe97d955afc17c019621a94a37590fd0eb1fa48
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-03-21 18:33:42 -0400

    Implemented methods from openxr_program.h in hello_xr

--- PortableEngine/XRAPI.cpp
+++ PortableEngine/XRAPI.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 XRAPI::XRAPI(IPlatform* plat, IGraphicsAPI* graph) : platform(plat), graphics(graph)
 {
+	Init();
 }
 
 XrResult XRAPI::Init()
@@ -21,22 +22,142 @@ XrResult XRAPI::Init()
 	xrEnumerateInstanceExtensionProperties(nullptr, 0, &nInstanceExtensionProps, nullptr);
 	std::vector<XrExtensionProperties> extensionProps(nInstanceExtensionProps, { XR_TYPE_EXTENSION_PROPERTIES });
 	xrEnumerateInstanceExtensionProperties(nullptr, nInstanceExtensionProps, &nInstanceExtensionProps, extensionProps.data());
-
-
+	res =  CreateXRInstance();
+	return res;
 }
 
 XrResult XRAPI::CreateXRInstance()
 {
+	std::vector<const char*> extensions;
+	const std::vector<const char*> platformExtensions = platform->GetXRPlatformPlugin()->GetPlatformExtensions();
+	std::transform(platformExtensions.begin(), platformExtensions.end(), std::back_inserter(extensions),
+		[](const char* ext) { return ext; });
+	const std::vector<const char*> graphicsExtensions = graphics->GetXRGraphicsPlugin()->GetGraphicsExtensions();
+	std::transform(graphicsExtensions.begin(), graphicsExtensions.end(), std::back_inserter(extensions),
+		[](const char* ext) { return ext; });
+
+
 	XrInstanceCreateInfo createInfo = { XR_TYPE_INSTANCE_CREATE_INFO };
+	createInfo.next = platform->GetXRPlatformPlugin()->GetInstanceCreateExtension();
 	createInfo.createFlags = 0;
 	createInfo.enabledApiLayerCount = 0;
 	createInfo.enabledApiLayerNames = nullptr;
-	createInfo.enabledExtensionCount = 1;
-	std::string glExtension = "XR_KHR_opengl_enable";
-	const char* cGlStr = glExtension.c_str();
+	createInfo.enabledExtensionCount = (uint32_t)extensions.size();
+	createInfo.enabledExtensionNames = extensions.data();
+	//std::string glExtension = "XR_KHR_opengl_enable";
+	//const char* cGlStr = graphics->GetXRGraphicsPlugin()->GetGraphicsExtensionName();
 	//createInfo.enabledExtensionNames = extensionNames.data();
-	createInfo.enabledExtensionNames = &cGlStr;
-	strcpy_s(createInfo.applicationInfo.applicationName, "HelloOpenXR");
+	//createInfo.enabledExtensionNames = &cGlStr;
+	strcpy_s(createInfo.applicationInfo.applicationName, "Portable Engine");
 	createInfo.applicationInfo.apiVersion = XR_CURRENT_API_VERSION;
 	XrResult createInstanceResult = xrCreateInstance(&createInfo, &m_instance);
+	return createInstanceResult;
+}
+
+void XRAPI::InitializeXRSystem()
+{
+	XrSystemGetInfo systemInfo{ XR_TYPE_SYSTEM_GET_INFO };
+	systemInfo.formFactor = XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY;
+	m_environmentBlendMode = XR_ENVIRONMENT_BLEND_MODE_OPAQUE;
+	xrGetSystem(m_instance, &systemInfo, &m_systemId);
+}
+
+void XRAPI::InitializeXRSession()
+{
+	XrSessionCreateInfo createInfo{ XR_TYPE_SESSION_CREATE_INFO };
+	createInfo.next = graphics->GetXRGraphicsPlugin()->GetGraphicsBinding();
+	createInfo.systemId = m_systemId;
+	xrCreateSession(m_instance, &createInfo, &m_session);
+
+
+	XrReferenceSpaceCreateInfo referenceSpaceCreateInfo{ XR_TYPE_REFERENCE_SPACE_CREATE_INFO };
+	XrPosef t;
+	t.orientation.w = 1;
+	referenceSpaceCreateInfo.poseInReferenceSpace = t;
+	xrCreateReferenceSpace(m_session, &referenceSpaceCreateInfo, &m_appSpace);
+
+
+}
+
+void XRAPI::CreateVisualizedSpaces()
+{
+
+}
+
+void XRAPI::CreateSwapchains()
+{
+	XrSystemProperties systemProperties{ XR_TYPE_SYSTEM_PROPERTIES };
+	xrGetSystemProperties(m_instance, m_systemId, &systemProperties);
+	uint32_t viewCount;
+	xrEnumerateViewConfigurationViews(m_instance, m_systemId, m_viewConfigType, 0, &viewCount, nullptr);
+	m_configViews.resize(viewCount, { XR_TYPE_VIEW_CONFIGURATION_VIEW });
+	xrEnumerateViewConfigurationViews(m_instance, m_systemId, m_viewConfigType, viewCount, &viewCount,
+		m_configViews.data());
+
+	// Create and cache view buffer for xrLocateViews later.
+	m_views.resize(viewCount, { XR_TYPE_VIEW });
+
+	// Create the swapchain and get the images.
+	if (viewCount > 0) {
+		// Select a swapchain format.
+		uint32_t swapchainFormatCount;
+		xrEnumerateSwapchainFormats(m_session, 0, &swapchainFormatCount, nullptr);
+		std::vector<int64_t> swapchainFormats(swapchainFormatCount);
+		xrEnumerateSwapchainFormats(m_session, (uint32_t)swapchainFormats.size(), &swapchainFormatCount,
+			swapchainFormats.data());
+		swapchainFormatCount == swapchainFormats.size();
+		m_colorSwapchainFormat = graphics->GetXRGraphicsPlugin()->SelectColorSwapchainFormat(swapchainFormats);
+
+		// Print swapchain formats and the selected one.
+		//{
+		//	std::string swapchainFormatsString;
+		//	for (int64_t format : swapchainFormats) {
+		//		const bool selected = format == m_colorSwapchainFormat;
+		//		swapchainFormatsString += " ";
+		//		if (selected) {
+		//			swapchainFormatsString += "[";
+		//		}
+		//		swapchainFormatsString += std::to_string(format);
+		//		if (selected) {
+		//			swapchainFormatsString += "]";
+		//		}
+		//	}
+			//Log::Write(Log::Level::Verbose, Fmt("Swapchain Formats: %s", swapchainFormatsString.c_str()));
+		}
+
+		// Create a swapchain for each view.
+		for (uint32_t i = 0; i < viewCount; i++) {
+			const XrViewConfigurationView& vp = m_configViews[i];
+			//Log::Write(Log::Level::Info,
+			//	Fmt("Creating swapchain for view %d with dimensions Width=%d Height=%d SampleCount=%d", i,
+			//		vp.recommendedImageRectWidth, vp.recommendedImageRectHeight, vp.recommendedSwapchainSampleCount));
+
+			// Create the swapchain.
+			XrSwapchainCreateInfo swapchainCreateInfo{ XR_TYPE_SWAPCHAIN_CREATE_INFO };
+			swapchainCreateInfo.arraySize = 1;
+			swapchainCreateInfo.format = m_colorSwapchainFormat;
+			swapchainCreateInfo.width = vp.recommendedImageRectWidth;
+			swapchainCreateInfo.height = vp.recommendedImageRectHeight;
+			swapchainCreateInfo.mipCount = 1;
+			swapchainCreateInfo.faceCount = 1;
+			// TODO: This method just returns a field in vp so I will do that directly for now. It might be a good idea to implement the method later though.
+			//swapchainCreateInfo.sampleCount = m_graphicsPlugin->GetSupportedSwapchainSampleCount(vp);
+			swapchainCreateInfo.sampleCount = vp.recommendedSwapchainSampleCount;
+			swapchainCreateInfo.usageFlags = XR_SWAPCHAIN_USAGE_SAMPLED_BIT | XR_SWAPCHAIN_USAGE_COLOR_ATTACHMENT_BIT;
+			Swapchain swapchain;
+			swapchain.width = swapchainCreateInfo.width;
+			swapchain.height = swapchainCreateInfo.height;
+			xrCreateSwapchain(m_session, &swapchainCreateInfo, &swapchain.handle);
+
+			m_swapchains.push_back(swapchain);
+
+			uint32_t imageCount;
+			xrEnumerateSwapchainImages(swapchain.handle, 0, &imageCount, nullptr);
+			// XXX This should really just return XrSwapchainImageBaseHeader*
+			std::vector<XrSwapchainImageBaseHeader*> swapchainImages =
+				graphics->GetXRGraphicsPlugin()->AllocateSwapchainImageStructs(imageCount, swapchainCreateInfo);
+			xrEnumerateSwapchainImages(swapchain.handle, imageCount, &imageCount, swapchainImages[0]);
+
+			m_swapchainImages.insert(std::make_pair(swapchain.handle, std::move(swapchainImages)));
+		}
 }
