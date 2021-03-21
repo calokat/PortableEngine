14218bd2118260f5262f265330bbcaf9d9203a44
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-03-21 18:30:29 -0400

    Declared most of the relevant methods for graphics plugins

--- PortableEngine/IXRGraphicsPlugin.h
+++ PortableEngine/IXRGraphicsPlugin.h
@@ -1,7 +1,12 @@
 #pragma once
 #include <vector>
 #include "xr_defines.h"
+#include "openxr/openxr.h"
 class IXRGraphicsPlugin {
 public:
 	virtual std::vector<const char*> GetGraphicsExtensions() = 0;
+	virtual void InitializeDeviceForXR(XrInstance instance, XrSystemId systemId) = 0;
+	virtual const XrBaseInStructure* GetGraphicsBinding() = 0;
+	virtual int64_t SelectColorSwapchainFormat(const std::vector<int64_t>& runtimeFormats) const = 0;
+	virtual std::vector<XrSwapchainImageBaseHeader*> AllocateSwapchainImageStructs(uint32_t capacity, const XrSwapchainCreateInfo& /*swapchainCreateInfo*/) = 0;
 };
