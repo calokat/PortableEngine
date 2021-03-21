a932792d5a609593b1b8d95ed2019abd3e328171
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-03-21 18:31:29 -0400

    Declared the methods in IXRGraphicsPlugin, as well as some necessary fields

--- PortableEngine/XRGraphicsPlugin_OpenGL.h
+++ PortableEngine/XRGraphicsPlugin_OpenGL.h
@@ -1,8 +1,32 @@
 #pragma once
 #include "IXRGraphicsPlugin.h"
+#define XR_USE_PLATFORM_WIN32 1
+#define XR_USE_GRAPHICS_API_OPENGL 1
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+#include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
+#include "GameWindow.h"
+#include "IOpenGLContext.h"
+#include <algorithm>
+#include <list>
 class XRGraphicsPlugin_OpenGL :
     public IXRGraphicsPlugin
 {
+public:
+    XRGraphicsPlugin_OpenGL(GameWindow* win, IOpenGLContext* ctx);
     std::vector<const char*> GetGraphicsExtensions();
+    void InitializeDeviceForXR(XrInstance instance, XrSystemId systemId);
+    const XrBaseInStructure* GetGraphicsBinding();
+    int64_t SelectColorSwapchainFormat(const std::vector<int64_t>& runtimeFormats) const;
+    std::vector<XrSwapchainImageBaseHeader*> AllocateSwapchainImageStructs(uint32_t capacity, const XrSwapchainCreateInfo& /*swapchainCreateInfo*/);
+private:
+#ifdef _WIN32
+    XrGraphicsBindingOpenGLWin32KHR m_graphicsBinding{ XR_TYPE_GRAPHICS_BINDING_OPENGL_WIN32_KHR };
+#endif
+    GameWindow* window;
+    IOpenGLContext* glContext;
+    std::list<std::vector<XrSwapchainImageOpenGLKHR>> m_swapchainImageBuffers;
 };
 
