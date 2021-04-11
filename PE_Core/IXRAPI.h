f59cf1f7f2400fb1f0ebdef7d19b75926a8a34b9
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-04-11 08:41:34 -0400

    Added virtual destructor to IXRAPI

--- PE_Core/IXRAPI.h
+++ PE_Core/IXRAPI.h
@@ -7,5 +7,6 @@ public:
     virtual void PollEvents() = 0;
     virtual bool IsSessionRunning() = 0;
     virtual void RenderFrame(entt::registry& reg, IRenderSystem* renderSystem) = 0;
+    virtual ~IXRAPI() {};
 };
 
