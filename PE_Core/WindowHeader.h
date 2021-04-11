897d83f886e352a88d20c7c408de1585447420a8
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-04-11 07:36:12 -0400

    Created WindowHeader, the GUI class that wraps the ImGui MainMenuBar

---
+++ PE_Core/WindowHeader.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <entt.hpp>
+#include "IAssetManager.h"
+#include "IRenderSystem.h"
+class WindowHeader
+{
+public:
+	void Render(entt::registry& registry, IAssetManager* assetManager, IRenderSystem* renderSystem);
+private:
+	void MakeMesh(const char* path, entt::registry& registry, IAssetManager* assetManager, IRenderSystem* renderSystem, const char* name = "GameObject");
+};
+
