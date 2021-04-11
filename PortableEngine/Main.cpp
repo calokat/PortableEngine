78da3d380688bb65cdf28bd245245f686d41cf03
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-04-11 07:36:50 -0400

    Implemented WindowHeader. The other GUI classes are no longer global variables, they are now local to Loop();

--- PortableEngine/Main.cpp
+++ PortableEngine/Main.cpp
@@ -39,6 +39,7 @@
 #include "EntityListWindow.h"
 #include "InspectorWindow.h"
 #include "AssetBrowserWindow.h"
+#include "WindowHeader.h"
 #include "XRAPI.h"
 #include "MockXRAPI.h"
 #include <thread>
@@ -61,9 +62,6 @@ GameWindow* window;
 IRenderSystem* renderSystem;
 entt::registry registry;
 
-EntityListWindow entityListWindow;
-InspectorWindow inspectorWindow;
-AssetBrowserWindow assetWindow;
 IXRAPI* xr;
 //template<class T>
 //void TrySerializeComponent(json& master)
@@ -305,78 +303,82 @@ void Loop()
 	ImGuizmo::BeginFrame();
 	ImGuizmo::Enable(true);
 	static float vertColorPick[4];
-	ImGui::BeginMainMenuBar();
-	if (ImGui::BeginMenu("File"))
-	{
-		if (ImGui::BeginMenu("New"))
-		{
-			if (ImGui::MenuItem("Cube"))
-			{
-				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/cube.obj").c_str(), "Cube");
-			}
-			if (ImGui::MenuItem("Helix"))
-			{
-				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/helix.obj").c_str(), "Helix");
-			}
-			if (ImGui::MenuItem("Cone"))
-			{
-				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/cone.obj").c_str(), "Cone");
-			}
-			if (ImGui::MenuItem("Cylinder"))
-			{
-				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/cylinder.obj").c_str(), "Cylinder");
-			}
-			if (ImGui::MenuItem("Sphere"))
-			{
-				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/sphere.obj").c_str(), "Sphere");
-			}
-			if (ImGui::MenuItem("Torus"))
-			{
-				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/torus.obj").c_str(), "Torus");
-			}
-			ImGui::EndMenu();
-		}
-		//if (ImGui::BeginMenu("Save"))
-//{
-//	static char saveFileName[100] = {};
-//	ImGui::InputText("Save as: ", saveFileName, 100);
-//	if (ImGui::Button("Save"))
+//	ImGui::BeginMainMenuBar();
+//	if (ImGui::BeginMenu("File"))
 //	{
-//		Serialize(saveFileName);
-//	}
-//	ImGui::EndMenu();
-//}
-//if (ImGui::BeginMenu("Open"))
-//{
-//	for (const auto& saveFile : std::filesystem::directory_iterator("./"))
-//	{
-//		if (saveFile.path().extension().generic_string() == ".pg")
+//		if (ImGui::BeginMenu("New"))
 //		{
-//			std::string fileStr = saveFile.path().generic_string();
-
-//			if (ImGui::MenuItem(fileStr.c_str()))
+//			if (ImGui::MenuItem("Cube"))
 //			{
-//				Deserialize(fileStr.c_str());
+//				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/cube.obj").c_str(), "Cube");
 //			}
+//			if (ImGui::MenuItem("Helix"))
+//			{
+//				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/helix.obj").c_str(), "Helix");
+//			}
+//			if (ImGui::MenuItem("Cone"))
+//			{
+//				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/cone.obj").c_str(), "Cone");
+//			}
+//			if (ImGui::MenuItem("Cylinder"))
+//			{
+//				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/cylinder.obj").c_str(), "Cylinder");
+//			}
+//			if (ImGui::MenuItem("Sphere"))
+//			{
+//				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/sphere.obj").c_str(), "Sphere");
+//			}
+//			if (ImGui::MenuItem("Torus"))
+//			{
+//				MakeMesh(plat->GetAssetManager()->GetAssetPath("../../Assets/Models/torus.obj").c_str(), "Torus");
+//			}
+//			ImGui::EndMenu();
 //		}
+//		//if (ImGui::BeginMenu("Save"))
+////{
+////	static char saveFileName[100] = {};
+////	ImGui::InputText("Save as: ", saveFileName, 100);
+////	if (ImGui::Button("Save"))
+////	{
+////		Serialize(saveFileName);
+////	}
+////	ImGui::EndMenu();
+////}
+////if (ImGui::BeginMenu("Open"))
+////{
+////	for (const auto& saveFile : std::filesystem::directory_iterator("./"))
+////	{
+////		if (saveFile.path().extension().generic_string() == ".pg")
+////		{
+////			std::string fileStr = saveFile.path().generic_string();
+//
+////			if (ImGui::MenuItem(fileStr.c_str()))
+////			{
+////				Deserialize(fileStr.c_str());
+////			}
+////		}
+////	}
+////	ImGui::EndMenu();
+////}
+//		ImGui::EndMenu();
 //	}
-//	ImGui::EndMenu();
-//}
-		ImGui::EndMenu();
-	}
-	ImGui::EndMainMenuBar();
+//	ImGui::EndMainMenuBar();
 
 	// TODO: Put the picking code in its own system
 	if (plat->GetInputSystem()->WasMouseButtonClicked(MouseButton::Left))
 	{
 		MakeRayFromCamera();
 	}
-
+	
 	auto entityView = registry.view<Name, Transform>();
+	EntityListWindow entityListWindow;
+	InspectorWindow inspectorWindow;
+	AssetBrowserWindow assetWindow;
+	WindowHeader windowHeader;
+
+	windowHeader.Render(registry, plat->GetAssetManager(), renderSystem);
 	entityListWindow.Render(entityView);
-
 	inspectorWindow.Render(registry);
-
 	assetWindow.Render(plat->GetAssetManager(), renderSystem);
 
 	graph->ClearScreen();
