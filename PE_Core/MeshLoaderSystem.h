538ef7282e69664104491f72d50c933cd3a6fbea
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-05-29 17:58:36 -0400

    Implemented assimp

--- PE_Core/MeshLoaderSystem.h
+++ PE_Core/MeshLoaderSystem.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "Vertex.h"
 #include "Mesh.h"
+#include <assimp\Importer.hpp>
+#include <assimp\scene.h>
+#include <assimp\postprocess.h>
+
 class MeshLoaderSystem
 {
 public:
 	static void LoadMesh(const char* fileName, Mesh& m);
-	static void CalculateTangents(Vertex* verts, int numVerts, unsigned int* indices, int numIndices);
+	static void ProcessMesh(aiMesh* assimpMesh, const aiScene* scene, Mesh& peMesh);
 };
 
