23be778107fac46734f029ccd375170864764fc2
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-07-10 12:24:35 -0400

    Following Rule of Zero with Mesh class

--- PE_Core/Mesh.h
+++ PE_Core/Mesh.h
@@ -5,15 +5,8 @@
 class Mesh
 {
 public:
-	Mesh(void);
-	Mesh(const char* fileName);
 	int numIndices;
 	std::string path;
 	std::vector<Vertex> rawVertices;
 	std::vector<unsigned int> rawIndices;
-	// Move assignment operator
-	Mesh& operator=(Mesh&& other);
-	// Move constructor
-	Mesh(Mesh&& other) noexcept;
-	Mesh(Mesh& other);
 };
