73643ea7b8dde2dc07260c66ce0f4425615c10ca
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-07-10 12:42:28 -0400

    EntityListWindow now able to render a tree of named components

--- PE_Core/EntityListWindow.cpp
+++ PE_Core/EntityListWindow.cpp
@@ -1,20 +1,42 @@
 #include "EntityListWindow.h"
 #include <imgui.h>
 #include "GizmoSystem.h"
-void EntityListWindow::Render(entt::basic_view<entt::entity, entt::exclude_t<>, Name, Transform> namedEntities)
+void EntityListWindow::Render(Scene<entt::entity> namedEntities, entt::basic_view<entt::entity, entt::exclude_t<>, Name> nameView)
 {
 	ImGui::Begin("Entity List");
 	ImGui::SetWindowPos({ 0, 20 });
 	ImGui::SetWindowSize({ 200, 780 });
-	for (auto entity : namedEntities)
+
+	SetUpGuiTree(namedEntities, nameView);
+	
+	ImGui::End();
+}
+
+void EntityListWindow::SetUpGuiTree(Scene<entt::entity> entityTree, entt::basic_view<entt::entity, entt::exclude_t<>, Name> nameView)
+{
+	Name entityName = nameView.get<Name>(entityTree.data);
+	if (entityTree.children.size() == 0)
 	{
-		Name name = namedEntities.get<Name>(entity);
-		if (ImGui::MenuItem(name.nameString.c_str()))
+		if (ImGui::MenuItem(entityName.nameString.c_str()))
 		{
 			GizmoSystem::DeselectAll();
-			Transform& newSelected = namedEntities.get<Transform>(entity);
-			GizmoSystem::Select(entity);
+			GizmoSystem::Select(entityTree.data);
 		}
 	}
-	ImGui::End();
+	else
+	{
+		if (ImGui::TreeNodeEx(entityName.nameString.c_str(), ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick))
+		{
+			if (ImGui::IsItemClicked())
+			{
+				GizmoSystem::DeselectAll();
+				GizmoSystem::Select(entityTree.data);
+			}
+			for (auto child : entityTree.children)
+			{
+				SetUpGuiTree(child, nameView);
+			}
+			ImGui::TreePop();
+		}
+	}
 }
--- PE_Core/EntityListWindow.h
+++ PE_Core/EntityListWindow.h
@@ -2,9 +2,13 @@
 #include <entt.hpp>
 #include "misc_components.h"
 #include "Transform.h"
+#include "Scene.h"
+
 class EntityListWindow
 {
 public:
-	void Render(entt::basic_view<entt::entity, entt::exclude_t<>, Name, Transform> namedEntities);
+	void Render(Scene<entt::entity> namedEntities, entt::basic_view<entt::entity, entt::exclude_t<>, Name> nameView);
+private:
+	void SetUpGuiTree(Scene<entt::entity> entityTree, entt::basic_view<entt::entity, entt::exclude_t<>, Name> nameView);
 };
 
