b8f89a0a61a88797ceada08feae2752a9707c631
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-07-10 12:40:22 -0400

    Refactored MeshLoaderSystem so that its CreateMeshHeirarchy method can return a tree of mesh data

--- PE_Core/MeshLoaderSystem.cpp
+++ PE_Core/MeshLoaderSystem.cpp
@@ -2,19 +2,7 @@
 #include <fstream>
 #pragma warning(disable : 4996)
 
-void MeshLoaderSystem::LoadMesh(const char* fileName, Mesh& m)
-{
-	Assimp::Importer imp;
-	const aiScene* scene = imp.ReadFile(fileName, aiProcess_Triangulate | aiProcess_CalcTangentSpace);
-	if (scene == nullptr) return;
-	for (unsigned int i = 0; i < scene->mNumMeshes; ++i)
-	{
-		ProcessMesh(scene->mMeshes[i], scene, m);
-	}
-
-}
-
-void MeshLoaderSystem::ProcessMesh(aiMesh* assimpMesh, const aiScene* scene, Mesh& peMesh)
+void MeshLoaderSystem::ProcessMesh(aiMesh* assimpMesh, const aiScene* scene, Mesh& m)
 {
 	for (unsigned int i = 0; i < assimpMesh->mNumVertices; ++i)
 	{
@@ -26,7 +14,7 @@ void MeshLoaderSystem::ProcessMesh(aiMesh* assimpMesh, const aiScene* scene, Mes
 			vertex.UV.x = (float)assimpMesh->mTextureCoords[0][i].x;
 			vertex.UV.y = (float)assimpMesh->mTextureCoords[0][i].y;
 		}
-		peMesh.rawVertices.push_back(vertex);
+		m.rawVertices.push_back(vertex);
 	}
 	for (uint32_t i = 0; i < assimpMesh->mNumFaces; ++i)
 	{
@@ -34,8 +22,43 @@ void MeshLoaderSystem::ProcessMesh(aiMesh* assimpMesh, const aiScene* scene, Mes
 
 		for (uint32_t j = 0; j < face.mNumIndices; ++j)
 		{
-			peMesh.rawIndices.push_back(face.mIndices[j]);
+			m.rawIndices.push_back(face.mIndices[j]);
 		}
 	}
-	peMesh.numIndices = peMesh.rawIndices.size();
+	m.path = assimpMesh->mName.C_Str();
+	m.numIndices = m.rawIndices.size();
+}
+
+Scene<MeshCreateInfo> MeshLoaderSystem::CreateMeshHeirarchy(const char* path)
+{
+	Scene<MeshCreateInfo> res;
+	Assimp::Importer imp;
+	const aiScene* meshScene = imp.ReadFile(path, aiProcess_Triangulate | aiProcess_CalcTangentSpace | aiProcess_EmbedTextures);
+	const aiNode* root = meshScene->mRootNode;
+	MeshLoaderSystem::AddToMeshHeirarchy(res, root, meshScene);
+	return res;
+}
+
+void MeshLoaderSystem::AddToMeshHeirarchy(Scene<MeshCreateInfo>& parent, const aiNode* assimpNode, const aiScene* assimpScene)
+{
+	aiVector3D pos, scale;
+	aiQuaternion rot;
+	assimpNode->mTransformation.Decompose(scale, rot, pos);
+	parent.data.t.position = glm::vec3(pos.x, pos.y, pos.z);
+	parent.data.t.orientation = glm::quat(rot.w, rot.x, rot.y, rot.z);
+	parent.data.t.scale = glm::vec3(scale.x, scale.y, scale.z);
+	if (assimpNode->mNumMeshes == 0)
+	{
+		parent.isEmpty = true;
+		parent.data.m.path = assimpNode->mName.C_Str();
+	}
+	for (int i = 0; i < assimpNode->mNumMeshes; ++i)
+	{
+		ProcessMesh(assimpScene->mMeshes[assimpNode->mMeshes[i]], assimpScene, parent.data.m);
+	}
+	parent.children.resize(assimpNode->mNumChildren);
+	for (int j = 0; j < assimpNode->mNumChildren; ++j)
+	{
+		AddToMeshHeirarchy(parent.children[j], assimpNode->mChildren[j], assimpScene);
+	}
 }
--- PE_Core/MeshLoaderSystem.h
+++ PE_Core/MeshLoaderSystem.h
@@ -4,11 +4,18 @@
 #include <assimp\Importer.hpp>
 #include <assimp\scene.h>
 #include <assimp\postprocess.h>
-
+#include "Transform.h"
+#include "Scene.h"
+struct MeshCreateInfo
+{
+	Mesh m;
+	Transform t;
+};
 class MeshLoaderSystem
 {
 public:
-	static void LoadMesh(const char* fileName, Mesh& m);
 	static void ProcessMesh(aiMesh* assimpMesh, const aiScene* scene, Mesh& peMesh);
+	static Scene<MeshCreateInfo> CreateMeshHeirarchy(const char* path);
+	static void AddToMeshHeirarchy(Scene<MeshCreateInfo>& parent, const aiNode* assimpNode, const aiScene* assimpScene);
 };
 
