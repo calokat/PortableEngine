045c2db20c1ce15aac6ef49c1f8410f6432ca617
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-12-25 15:21:37 -0500

    - GLRenderer uses OpenGLShaderProgram - Deleted lots of commented code

--- PE_OpenGL/GLRenderer.cpp
+++ PE_OpenGL/GLRenderer.cpp
@@ -4,110 +4,17 @@
 #include <glm/gtc/type_ptr.hpp>
 GLRenderer::GLRenderer(std::string vertexShaderPathParam, std::string fragmentShaderPathParam)
 {
-	//glGenVertexArrays(1, &vao);
-	//glBindVertexArray(vao);
-	//glGenBuffers(1, &vbo);
-	//glGenBuffers(1, &ibo);
-	//glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	this->vertexShaderPath = vertexShaderPathParam;
-	this->fragmentShaderPath = fragmentShaderPathParam;
-#ifndef __EMSCRIPTEN__
-	vertex = Shader(vertexShaderPathParam, GL_VERTEX_SHADER);
-#else
-	vertex = Shader(vertexShaderPathParam, GL_VERTEX_SHADER);
-#endif
-#ifndef __EMSCRIPTEN__
-	pixel = Shader(fragmentShaderPathParam, GL_FRAGMENT_SHADER);
-#else
-	pixel = Shader(fragmentShaderPathParam, GL_FRAGMENT_SHADER);
-#endif
-	vertex.Compile();
-	pixel.Compile();
-	//program = glCreateProgram();
-	//glAttachShader(program, vertex->GetId());
-	//glAttachShader(program, pixel->GetId());
-	//glLinkProgram(program);
-	//glUseProgram(program);
-	//projLoc = glGetUniformLocation(program, "projection");
-	//viewLoc = glGetUniformLocation(program, "view");
-	//modelLoc = glGetUniformLocation(program, "model");
-	//glm::mat4 model(1.0f);
-	//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-	//glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(camera->GetViewMatrix()));
-	//glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(camera->GetProjectionMatrix()));
+	shaderProgram.vertex = Shader(vertexShaderPathParam, GL_VERTEX_SHADER);
+	shaderProgram.pixel = Shader(fragmentShaderPathParam, GL_FRAGMENT_SHADER);
+	shaderProgram.vertex.Compile();
+	shaderProgram.pixel.Compile();
+	shaderProgram.attributes = { {"in_position", {} }, {"aTexCoord", {} } };
+	shaderProgram.uniforms = { {"projection", {} }, {"view", {} }, {"model", {} }, {"in_color", {} } };
 }
 
 GLRenderer::GLRenderer()
 {
-	program = -1;
-	vao = -1;
-	vbo = -1;
-	ibo = -1;
 	numVertices = -1;
 	numIndices = 0;
 }
 
-//void Renderer::LoadMesh(std::vector<Vertex> rawVertices)
-//{
-//	glBindVertexArray(vao);
-//	glBufferData(GL_ARRAY_BUFFER, rawVertices.size() * sizeof(Vertex), rawVertices.data(), GL_STATIC_DRAW);
-//	GLint posAttrib = glGetAttribLocation(program, "in_position");
-//	glEnableVertexAttribArray(posAttrib);
-//	SetupAttribute(posAttrib, 3, GL_FLOAT, Vertex, Position);
-//	numVertices = rawVertices.size();
-//	glBindVertexArray(0);
-//}
-
-//void Renderer::Draw()
-//{
-//	glBindVertexArray(vao);
-//	glDrawArrays(GL_TRIANGLES, 0, numVertices);
-//	glBindVertexArray(0);
-//}
-
-//void Renderer::Update()
-//{
-//	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(camera->GetViewMatrix()));
-//	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(camera->GetProjectionMatrix()));
-//}
-
-GLRenderer::~GLRenderer()
-{
-}
-
-GLRenderer& GLRenderer::operator=(GLRenderer&& other)
-{
-	if (this != &other)
-	{
-		this->vertex = other.vertex;
-		this->pixel = other.pixel;
-		this->vao = other.vao;
-		this->vbo = other.vbo;
-		this->ibo = other.ibo;
-		this->program = other.program;
-		this->projLoc = other.projLoc;
-		this->viewLoc = other.viewLoc;
-		this->modelLoc = other.modelLoc;
-		this->colorLoc = other.colorLoc;
-		this->numVertices = other.numVertices;
-		this->vertexShaderPath = other.vertexShaderPath;
-		this->fragmentShaderPath = other.fragmentShaderPath;
-		this->vertexColor = other.vertexColor;
-		this->diffuseTexture = std::move(other.diffuseTexture);
-		this->numIndices = other.numIndices;
-	}
-	return *this;
-}
-
-GLRenderer::GLRenderer(GLRenderer&& other) noexcept
-{
-	*this = std::move(other);
-}
-
-GLRenderer::GLRenderer(GLRenderer& other)
-{
-	if (this != &other)
-	{
-		*this = GLRenderer(other.vertexShaderPath, other.fragmentShaderPath);
-	}
-}
--- PE_OpenGL/GLRenderer.h
+++ PE_OpenGL/GLRenderer.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include "Vertex.h"
 #include "IRenderer.h"
+#include "OpenGLShaderProgram.h"
 // source: https://github.com/IGME-RIT/Basic-OpenGL-with-GLFW-Assimp/blob/master/OpenGLObjectLoading/mesh.cpp
 #define SetupAttribute(index, size, type, structure, element) \
 	glVertexAttribPointer(index, size, type, 0, sizeof(structure), (void*)offsetof(structure, element)); \
@@ -15,27 +16,11 @@
 class GLRenderer : public IRenderer
 {
 public:
-	Shader vertex;
-	Shader pixel;
-	//IGraphicsAPI* glAPI;
-	GLuint program;
-	GLuint vao, vbo, ibo;
-	GLint projLoc, viewLoc, modelLoc, colorLoc;
 	int numVertices;
-	std::string vertexShaderPath;
-	std::string fragmentShaderPath;
 	unsigned int numIndices;
 	GLRenderer(std::string vertexShaderPathParam, std::string fragmentShaderPathParam);
 	GLRenderer();
-	//void LoadMesh(std::vector<Vertex>);
-	//void Draw();
-	//void Update();
-	~GLRenderer();
-	// Move assignment operator
-	GLRenderer& operator=(GLRenderer&& other);
-	// Move constructor
-	GLRenderer (GLRenderer&& other) noexcept;
-	GLRenderer(GLRenderer& other);
 	glm::vec4 vertexColor = { 1, 1, 1, 1 };
 	PEImage diffuseTexture;
+	OpenGLShaderProgram shaderProgram;
 };
