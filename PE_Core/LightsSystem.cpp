e0bba5367600e89bfbcd1b9024344606a7cec4d0
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-01-08 22:37:16 -0500

    Added CreatePointLight function

--- PE_Core/LightsSystem.cpp
+++ PE_Core/LightsSystem.cpp
@@ -1,5 +1,8 @@
 #include "LightsSystem.h"
-
+#include "Relationship.h"
+#include "MeshMaker.h"
+#include "BillboardSystem.h"
+#include "misc_components.h"
 namespace LightsSystem
 {
 	void LightsSystem::ExtractLightsFromRegistry(entt::registry& reg, DirectionalLight& dirLight, PointLight* pointLights)
@@ -13,5 +16,21 @@ namespace LightsSystem
 		{
 			pointLights[i] = pointLightView.get<PointLight>(*it);
 		}
+	}
+	entt::entity CreatePointLight(entt::registry& reg, Tree<MeshCreateInfo> billBoardCreateInfo, IRenderSystem* renderSystem, IAssetManager* assetManager)
+	{
+		entt::entity res = reg.create();
+		PointLight& pl = reg.emplace<PointLight>(res);
+		reg.emplace<Name>(res, "Light Test");
+		Transform& plTransform = reg.emplace<Transform>(res);
+		reg.emplace<Relationship>(res);
+		entt::entity billBoardEntity = MakeMesh(billBoardCreateInfo, reg, res);
+		reg.emplace<Billboard>(billBoardEntity);
+		Relationship& billBoardRel = reg.get<Relationship>(billBoardEntity);
+		for (auto it = billBoardRel.children.begin(); it != billBoardRel.children.end(); ++it)
+		{
+			AttachRenderers(reg, renderSystem, assetManager->GetAssetPath("../../Assets/Images/light_bulb.jpg").c_str(), it->second, ShaderType::Unlit_Textured);
+		}
+		return res;
 	}
 }
--- PE_Core/LightsSystem.h
+++ PE_Core/LightsSystem.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "lights.h"
 #include <entt.hpp>
-
+#include "MeshCreateInfo.h"
+#include "Tree.h"
+#include "IRenderSystem.h"
+#include "IAssetManager.h"
 namespace LightsSystem
 {
 	void ExtractLightsFromRegistry(entt::registry& reg, DirectionalLight& dirLight, PointLight* pointLights);
+	entt::entity CreatePointLight(entt::registry& reg, Tree<MeshCreateInfo> billBoardCreateInfo, IRenderSystem* renderSystem, IAssetManager* assetManager);
 }
