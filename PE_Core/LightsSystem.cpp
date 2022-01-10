498dfcd9ac3e47cd0769056a5c7603365c7bc70c
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-01-09 19:22:18 -0500

    Steal AABB from plane mesh

--- PE_Core/LightsSystem.cpp
+++ PE_Core/LightsSystem.cpp
@@ -3,6 +3,7 @@
 #include "MeshMaker.h"
 #include "BillboardSystem.h"
 #include "misc_components.h"
+#include "AABBSystem.h"
 namespace LightsSystem
 {
 	void LightsSystem::ExtractLightsFromRegistry(entt::registry& reg, DirectionalLight& dirLight, PointLight* pointLights)
@@ -26,13 +27,17 @@ namespace LightsSystem
 		reg.emplace<Name>(res, "Light Test");
 		Transform& plTransform = reg.emplace<Transform>(res);
 		reg.emplace<Relationship>(res);
-		entt::entity billBoardEntity = MakeMesh(billBoardCreateInfo, reg, res);
+		entt::entity billBoardEntity = MakeMesh_Recursive(reg, billBoardCreateInfo, res, false);
 		reg.emplace<Billboard>(billBoardEntity);
 		Relationship& billBoardRel = reg.get<Relationship>(billBoardEntity);
 		for (auto it = billBoardRel.children.begin(); it != billBoardRel.children.end(); ++it)
 		{
 			AttachRenderers(reg, renderSystem, assetManager->GetAssetPath("../../Assets/Images/light_bulb.jpg").c_str(), it->second, ShaderType::Unlit_Textured);
 		}
+		Mesh planeMesh = billBoardCreateInfo.children[0].data.m;
+		AABB bb = {};
+		AABBSystem::UpdateAABB(bb, planeMesh, plTransform);
+		reg.emplace<AABB>(res, bb);
 		return res;
 	}
 	void LoadPointLightPositions(entt::basic_view<entt::entity, entt::exclude_t<>, PointLight, Transform> pointLights)
