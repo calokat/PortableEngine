eb4ea336ed61854546634e77bed643399117d97f
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-01-16 16:03:35 -0500

    Removed BillboardSystem usage (for now)

--- PE_Core/loop.cpp
+++ PE_Core/loop.cpp
@@ -108,7 +108,6 @@ void Loop(IPlatform* plat, IGraphicsAPI* graph, IRenderSystem* renderSystem, IXR
 	TransformSystem::CalculateWorldMatrix(&camTransform);
 	CameraSystem::CalculateViewMatrixLH(camera, camTransform);
 
-	BillboardSystem::SetBillboardTransforms(registry.view<Billboard, Transform>(), camera);
 
 	// TODO: Put the picking code in its own system
 	if (plat->GetInputSystem()->WasMouseButtonClicked(MouseButton::Left))
