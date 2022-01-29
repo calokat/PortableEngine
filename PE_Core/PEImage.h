fcb1809acf82ad9eccc35b096d0fee28f528f602
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-01-29 17:25:22 -0500

    - Deleted PEImage.cpp - Added pathChanged bool field

--- PE_Core/PEImage.h
+++ PE_Core/PEImage.h
@@ -4,17 +4,12 @@
 #include <memory>
 struct PEImage
 {
+	std::string path;
 	int width;
 	int height;
 	int numChannels;
-	std::string path;
 	unsigned char* data;
 	std::shared_ptr<IImageGraphicsData> imageGraphicsData;
-	PEImage();
-	PEImage(std::string p);
-	PEImage(PEImage& other);
-	PEImage& operator=(PEImage& other);
-	PEImage(PEImage&& other);
-	PEImage& operator=(PEImage&& other);
+	bool pathChanged;
 	//~PEImage();
 };
