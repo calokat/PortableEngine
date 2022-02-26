ae6f9dc5315695578168c83ea8d13abb4af46a93
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-02-25 21:51:03 -0500

    Deleted unused custom imgui files

--- PortableEngine/imgui_impl_emscripten.h
+++
@@ -1,8 +0,0 @@
-#ifdef __EMSCRIPTEN__
-#pragma once
-#include "GameWindow.h"
-#include "IInputSystem.h"
-bool ImGui_ImplEmscripten_Init(GameWindow* win, IInputSystem* in);
-void ImGui_ImplEmscripten_Shutdown();
-void ImGui_ImplEmscripten_NewFrame();
-#endif
