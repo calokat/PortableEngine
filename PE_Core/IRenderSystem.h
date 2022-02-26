aaa3b7fd8d6776a03c61a8c9b48b3987c1d462d9
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-02-25 21:54:11 -0500

    Moved ShaderType enum to IRenderer

--- PE_Core/IRenderSystem.h
+++ PE_Core/IRenderSystem.h
@@ -8,15 +8,6 @@
 #include "lights.h"
 #include <map>
 
-enum ShaderType
-{
-	Unlit_Color = ShaderProgramProperties::Colored | ShaderProgramProperties::Fully_3D,
-	Unlit_Textured = ShaderProgramProperties::Fully_3D | ShaderProgramProperties::Textured | ShaderProgramProperties::Colored,
-	Lit_Color = ShaderProgramProperties::Fully_3D | ShaderProgramProperties::Colored | ShaderProgramProperties::Lit,
-	Lit_Textured = ShaderProgramProperties::Fully_3D | ShaderProgramProperties::Colored | ShaderProgramProperties::Lit | ShaderProgramProperties::Textured,
-	Lit_Textured_Normal = ShaderProgramProperties::Fully_3D | ShaderProgramProperties::Colored | ShaderProgramProperties::Lit | ShaderProgramProperties::Textured | ShaderProgramProperties::Normal
-};
-
 class IRenderSystem
 {
 public:
--- PE_Core/IRenderer.h
+++ PE_Core/IRenderer.h
@@ -3,6 +3,15 @@
 
 enum ShaderProgramProperties { Fully_3D = 1, Colored = 2, Textured = 4, Lit = 8, Normal = 16 };
 
+enum ShaderType
+{
+	Unlit_Color = ShaderProgramProperties::Colored | ShaderProgramProperties::Fully_3D,
+	Unlit_Textured = ShaderProgramProperties::Fully_3D | ShaderProgramProperties::Textured | ShaderProgramProperties::Colored,
+	Lit_Color = ShaderProgramProperties::Fully_3D | ShaderProgramProperties::Colored | ShaderProgramProperties::Lit,
+	Lit_Textured = ShaderProgramProperties::Fully_3D | ShaderProgramProperties::Colored | ShaderProgramProperties::Lit | ShaderProgramProperties::Textured,
+	Lit_Textured_Normal = ShaderProgramProperties::Fully_3D | ShaderProgramProperties::Colored | ShaderProgramProperties::Lit | ShaderProgramProperties::Textured | ShaderProgramProperties::Normal
+};
+
 class IRenderer
 {
 public:
