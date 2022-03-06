40927a61ea578dd065f417165ed0fe85575b3c9c
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-03-05 20:01:48 -0500

    Manually copied and pasted ShaderHeader into shaders

--- PE_Win32/PixelShader_LitTextured.hlsl
+++ PE_Win32/PixelShader_LitTextured.hlsl
@@ -1,4 +1,61 @@
-#include "ShaderHeader.hlsli"
+// structs
+struct DirectionalLight
+{
+	float4 AmbientColor;
+	float4 DiffuseColor;
+	float4 Direction;
+};
+struct PointLight
+{
+	float4 AmbientColor;
+	float4 DiffuseColor;
+	float4 Position;
+};
+struct VertexShaderInput
+{
+	// Data type
+	//  |
+	//  |   Name          Semantic
+	//  |    |                |
+	//  v    v                v
+	float3 position		: POSITION;     // XYZ position
+	float3 normal		: NORMAL;
+	float2 UV			: TEXCOORD;
+	float3 tangent		: TANGENT;
+};
+// Struct representing the data we expect to receive from earlier pipeline stages
+// - Should match the output of our corresponding vertex shader
+// - The name of the struct itself is unimportant
+// - The variable names don't have to match other shaders (just the semantics)
+// - Each variable must have a semantic, which defines its usage
+struct VertexToPixel
+{
+	// Data type
+	//  |
+	//  |   Name          Semantic
+	//  |    |                |
+	//  v    v                v
+	float4 position		: SV_POSITION;
+	float4 color		: COLOR;
+	float3 normal		: NORMAL;
+	float3 worldPos		: POSITION;
+	float2 UV			: TEXCOORD;
+};
+
+struct VertexToPixelNormalMap
+{
+	// Data type
+	//  |
+	//  |   Name          Semantic
+	//  |    |                |
+	//  v    v                v
+	float4 position		: SV_POSITION;
+	float4 color		: COLOR;
+	float3 normal		: NORMAL;
+	float3 worldPos		: POSITION;
+	float2 UV			: TEXCOORD;
+	float3 tangent		: TANGENT;
+};
 
 cbuffer LightData: register(b0)
 {
