4aaa9e4058f6af3e41e3e9d07587643af3be4b9f
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2022-03-08 16:23:42 -0500

    Window Header is much simpler, does less

--- PE_Core/WindowHeader.cpp
+++ PE_Core/WindowHeader.cpp
@@ -4,8 +4,38 @@
 #include "TransformSystem.h"
 #include "misc_components.h"
 #include "AABBSystem.h"
-void WindowHeader::Render(entt::registry& registry, IAssetManager* assetManager, IRenderSystem* renderSystem)
+const char* WindowHeader::Render()
 {
 	ImGui::BeginMainMenuBar();
+	char* res = nullptr;
+	if (ImGui::BeginMenu("Import Mesh"))
+	{
+		if (ImGui::MenuItem("Cube"))
+		{
+			res = "../../Assets/Models/cube.obj";
+		}
+		if (ImGui::MenuItem("Sphere"))
+		{
+			res = "../../Assets/Models/sphere.obj";
+		}
+		if (ImGui::MenuItem("Cone"))
+		{
+			res = "../../Assets/Models/cone.obj";
+		}
+		if (ImGui::MenuItem("Torus"))
+		{
+			res = "../../Assets/Models/torus.obj";
+		}
+		if (ImGui::MenuItem("Helix"))
+		{
+			res = "../../Assets/Models/helix.obj";
+		}
+		if (ImGui::MenuItem("Cylinder"))
+		{
+			res = "../../Assets/Models/cylinder.obj";
+		}
+		ImGui::EndMenu();
+	}
 	ImGui::EndMainMenuBar();
+	return res;
 }
--- PE_Core/WindowHeader.h
+++ PE_Core/WindowHeader.h
@@ -5,8 +5,6 @@
 class WindowHeader
 {
 public:
-	void Render(entt::registry& registry, IAssetManager* assetManager, IRenderSystem* renderSystem);
-private:
-	void MakeMesh(const char* path, entt::registry& registry, IAssetManager* assetManager, IRenderSystem* renderSystem, const char* name = "GameObject");
+	const char* Render();
 };
 
